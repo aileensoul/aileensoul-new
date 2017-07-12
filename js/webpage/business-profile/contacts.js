7ba2cbd81d4da04c4fbf8560a8b5dd70fb44beff
DESKTOP-FNQCPB7\A <ankit.aileensoul@gmail.com>
2017-07-12 19:43:53 +0530

    Business Profile Updated Work

---
+++ js/webpage/business-profile/contacts.js
@@ -0,0 +1,303 @@
+$(function () {
+    $("#tags").autocomplete({
+        source: function (request, response) {
+            var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(request.term), "i");
+            response($.grep(data, function (item) {
+                return matcher.test(item.label);
+            }));
+        },
+        minLength: 1,
+        select: function (event, ui) {
+            event.preventDefault();
+            $("#tags").val(ui.item.label);
+            $("#selected-tag").val(ui.item.label);
+            // window.location.href = ui.item.value;
+        }
+        ,
+        focus: function (event, ui) {
+            event.preventDefault();
+            $("#tags").val(ui.item.label);
+        }
+    });
+});
+$(function () {
+    $("#searchplace").autocomplete({
+        source: function (request, response) {
+            var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(request.term), "i");
+            response($.grep(data1, function (item) {
+                return matcher.test(item.label);
+            }));
+        },
+        minLength: 1,
+        select: function (event, ui) {
+            event.preventDefault();
+            $("#searchplace").val(ui.item.label);
+            $("#selected-tag").val(ui.item.label);
+        },
+        focus: function (event, ui) {
+            event.preventDefault();
+            $("#searchplace").val(ui.item.label);
+        }
+    });
+});
+function checkvalue() {
+    var searchkeyword = $.trim(document.getElementById('tags').value);
+    var searchplace = $.trim(document.getElementById('searchplace').value);
+    if (searchkeyword == "" && searchplace == "") {
+        return false;
+    }
+}
+
+function updateprofilepopup(id) {
+    $('#bidmodal-2').modal('show');
+}
+
+// UPLOAD COVER PIC START 
+function myFunction() {
+    document.getElementById("upload-demo").style.visibility = "hidden";
+    document.getElementById("upload-demo-i").style.visibility = "hidden";
+    document.getElementById('message1').style.display = "block";
+}
+
+function showDiv() {
+    document.getElementById('row1').style.display = "block";
+    document.getElementById('row2').style.display = "none";
+}
+
+jQuery.noConflict();
+
+(function ($) {
+    $uploadCrop = $('#upload-demo').croppie({
+        enableExif: true,
+        viewport: {
+            width: 1250,
+            height: 350,
+            type: 'square'
+        },
+        boundary: {
+            width: 1250,
+            height: 350
+        }
+    });
+
+    $('.upload-result').on('click', function (ev) {
+        $uploadCrop.croppie('result', {
+            type: 'canvas',
+            size: 'viewport'
+        }).then(function (resp) {
+
+            $.ajax({
+                url: base_url + "business_profile/ajaxpro",
+                type: "POST",
+                data: {"image": resp},
+                success: function (data) {
+                    html = '<img src="' + resp + '" />';
+                    if (html)
+                    {
+                        window.location.reload();
+                    }
+
+                }
+            });
+
+        });
+    });
+
+    $('.cancel-result').on('click', function (ev) {
+        document.getElementById('row2').style.display = "block";
+        document.getElementById('row1').style.display = "none";
+        document.getElementById('message1').style.display = "none";
+    });
+
+    $('#upload').on('change', function () {
+
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $uploadCrop.croppie('bind', {
+                url: e.target.result
+            }).then(function () {
+                console.log('jQuery bind complete');
+            });
+
+        }
+        reader.readAsDataURL(this.files[0]);
+    });
+
+    $('#upload').on('change', function () {
+
+        var fd = new FormData();
+        fd.append("image", $("#upload")[0].files[0]);
+        files = this.files;
+        size = files[0].size;
+        if (!files[0].name.match(/.(jpg|jpeg|png|gif)$/i)) {
+            picpopup();
+            document.getElementById('row1').style.display = "none";
+            document.getElementById('row2').style.display = "block";
+            $("#upload").val('');
+            return false;
+        }
+
+        if (size > 4194304)
+        {
+            //show an alert to the user
+            alert("Allowed file size exceeded. (Max. 4 MB)")
+
+            document.getElementById('row1').style.display = "none";
+            document.getElementById('row2').style.display = "block";
+            //reset file upload control
+            return false;
+        }
+
+        $.ajax({
+
+            url: base_url + "business_profile/imagedata",
+            type: "POST",
+            data: fd,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+
+
+            }
+        });
+    });
+})(jQuery);
+
+// UPLOAD COVER PIC START 
+// follow user script start 
+
+// follow user script END 
+function followuser(clicked_id)
+{
+    $.ajax({
+        type: 'POST',
+        url: base_url + "business_profile/follow",
+        data: 'follow_to=' + clicked_id,
+        success: function (data) {
+            $('.' + 'fr' + clicked_id).html(data);
+        }
+    });
+}
+// follow like script end 
+
+// Unfollow user script start 
+function unfollowuser(clicked_id)
+{
+    $.ajax({
+        type: 'POST',
+        url: base_url + "business_profile/unfollow",
+        data: 'follow_to=' + clicked_id,
+        success: function (data) {
+            $('.' + 'fr' + clicked_id).html(data);
+        }
+    });
+}
+// follow like script end 
+
+// script for profile pic strat 
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('preview').style.display = 'block';
+            $('#preview').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#profilepic").change(function () {
+    profile = this.files;
+    if (!profile[0].name.match(/.(jpg|jpeg|png|gif)$/i)) {
+        //alert('not an image');
+        $('#profilepic').val('');
+        picpopup();
+        return false;
+    } else {
+        readURL(this);
+    }
+});
+// script for profile pic end 
+
+function picpopup() {
+    $('.biderror .mes').html("<div class='pop_content'>This is not valid file. Please Uplode valid Image File.");
+    $('#bidmodal').modal('show');
+}
+
+$(document).on('keydown', function (e) {
+    if (e.keyCode === 27) {
+        $('#bidmodal-2').modal('hide');
+    }
+});
+
+// scroll page script start 
+// For Scroll page at perticular position js Start 
+$(document).ready(function () {
+    $('html,body').animate({scrollTop: 330}, 100);
+});
+//For Scroll page at perticular position js End
+// scroll page script end 
+// contact person script start 
+function contact_person(clicked_id) {
+    $.ajax({
+        type: 'POST',
+        url: base_url + "business_profile/contact_person",
+        data: 'toid=' + clicked_id,
+        success: function (data) {
+            $('#contact_per').html(data);
+        }
+    });
+}
+
+function contact_person_menu(clicked_id) {
+    $.ajax({
+        type: 'POST',
+        url: base_url + "business_profile/contact_person_menu",
+        data: 'toid=' + clicked_id,
+        success: function (data) {
+            $('#' + 'statuschange' + clicked_id).html(data);
+        }
+    });
+}
+// contact person script end 
+
+function removecontactuser(clicked_id) {
+    var showdata = window.location.href.split("/").pop();
+    $.ajax({
+        type: 'POST',
+        url: base_url + "business_profile/removecontactuser",
+        dataType: 'json',
+        data: 'contact_id=' + clicked_id + '&showdata=' + showdata,
+        success: function (data) {
+            $('#' + 'statuschange' + clicked_id).html(data.contactdata);
+            if (data.notfound == 1) {
+                if (data.notcount == 0) {
+                    $('.' + 'contact-frnd-post').html(data.nomsg);
+                } else {
+                    $('#' + 'removecontact' + clicked_id).fadeOut(4000);
+                }
+            }
+        }
+    });
+}
+
+function contact_person_cancle(clicked_id, status) {
+    if (status == 'confirm') {
+        $('.biderror .mes').html("<div class='pop_content'> Do you want to remove this user from your contact list?<div class='model_ok_cancel'><a class='okbtn' id=" + clicked_id + " onClick='removecontactuser(" + clicked_id + ")' href='javascript:void(0);' data-dismiss='modal'>Yes</a><a class='cnclbtn' href='javascript:void(0);' data-dismiss='modal'>No</a></div></div>");
+        $('#bidmodal').modal('show');
+    } else if (status == 'pending') {
+        $('.biderror .mes').html("<div class='pop_content'> Do you want to cancel  contact request?<div class='model_ok_cancel'><a class='okbtn' id=" + clicked_id + " onClick='contact_person_menu(" + clicked_id + ")' href='javascript:void(0);' data-dismiss='modal'>Yes</a><a class='cnclbtn' href='javascript:void(0);' data-dismiss='modal'>No</a></div></div>");
+        $('#bidmodal').modal('show');
+    }
+}
+
+function contact_person_model(clicked_id, status) {
+    if (status == 'pending') {
+        $('.biderror .mes').html("<div class='pop_content'> Do you want to cancel  contact request?<div class='model_ok_cancel'><a class='okbtn' id=" + clicked_id + " onClick='contact_person(" + clicked_id + ")' href='javascript:void(0);' data-dismiss='modal'>Yes</a><a class='cnclbtn' href='javascript:void(0);' data-dismiss='modal'>No</a></div></div>");
+        $('#bidmodal').modal('show');
+    } else if (status == 'confirm') {
+        $('.biderror .mes').html("<div class='pop_content'> Do you want to remove this user from your contact list?<div class='model_ok_cancel'><a class='okbtn' id=" + clicked_id + " onClick='contact_person(" + clicked_id + ")' href='javascript:void(0);' data-dismiss='modal'>Yes</a><a class='cnclbtn' href='javascript:void(0);' data-dismiss='modal'>No</a></div></div>");
+        $('#bidmodal').modal('show');
+    }
+}
