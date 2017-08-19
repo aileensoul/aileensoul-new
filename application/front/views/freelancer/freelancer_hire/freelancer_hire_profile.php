58f8d56802b532a93d4943a9e9b7e19951b64ef6
harshad-aileensoul <harshad.aileensoul@gmail.com>
2017-08-19 12:29:44 -0400

    css optimization

--- application/front/views/freelancer/freelancer_hire/freelancer_hire_profile.php
+++ application/front/views/freelancer/freelancer_hire/freelancer_hire_profile.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/timeline.css'); ?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/css/croppie.css'); ?>">
         <link rel="stylesheet" href="<?php echo base_url('css/bootstrap.min.css'); ?>" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/profiles/freelancer-hire/freelancer-hire.css'); ?>">
         <style type="text/css">
             #popup-form img{display: none;}
         </style>
--- application/front/views/freelancer/freelancer_post/freelancer_post_profile.php
+++ application/front/views/freelancer/freelancer_post/freelancer_post_profile.php
@@ -9,6 +9,7 @@
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/timeline.css'); ?>">
         <link rel="stylesheet" href="<?php echo base_url('css/bootstrap.min.css'); ?>" />
         <link rel="stylesheet" href="<?php echo base_url('assets/css/croppie.css'); ?>">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/profiles/freelancer-apply/freelancer-apply.css'); ?>">
     </head>
     <body class="page-container-bg-solid page-boxed">
         <?php echo $header; ?>
