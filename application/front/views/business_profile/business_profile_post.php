78eeb7fed42b194d4116fe37c42fbbe71ea34c0d
DESKTOP-FNQCPB7\A <ankit.aileensoul@gmail.com>
2017-09-01 12:41:13 +0530

    changes

--- application/front/views/business_profile/business_profile_post.php
+++ application/front/views/business_profile/business_profile_post.php
@@ -326,7 +326,7 @@
 <?php echo $footer; ?>
                     </footer>
                     <script src="<?php echo base_url('js/jquery.wallform.js?ver='.time()); ?>"></script>
-                    <!--<script src="<?php // echo base_url('js/jquery-ui.min.js?ver='.time()); ?>"></script>-->
+                    <script src="<?php // echo base_url('js/jquery-ui.min.js?ver='.time()); ?>"></script>
                     <!--<script src="<?php // echo base_url('js/demo/jquery-1.9.1.js?ver='.time()); ?>"></script>--> 
                     <!--<script src="<?php // echo base_url('js/demo/jquery-ui-1.9.1.js?ver='.time()); ?>"></script>--> 
                     <script src="<?php echo base_url('js/bootstrap.min.js?ver='.time()); ?>"></script>
