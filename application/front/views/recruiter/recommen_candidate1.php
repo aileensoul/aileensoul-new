5f3b4a9d28bafab0657ab1ea398d09e8b2232fef
khyati@123456 <khyati.aileensoul@gmail.com>
2017-09-02 11:53:10 +0530

    recruiter

--- application/front/views/recruiter/recommen_candidate1.php
+++ application/front/views/recruiter/recommen_candidate1.php
@@ -15,7 +15,7 @@
 
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/timeline.css'); ?>">
 <link rel="stylesheet" href="<?php echo base_url('css/bootstrap.min.css'); ?>" />
-
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/profiles/recruiter/recruiter.css'); ?>">
 <script src="<?php echo base_url('js/fb_login.js'); ?>"></script>
 
 <!-- END HEADER -->
