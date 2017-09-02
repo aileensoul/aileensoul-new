93ab06db25b7206dfdefabff53aa9afb6c2052cd
DESKTOP-FNQCPB7\A <ankit.aileensoul@gmail.com>
2017-09-02 15:51:58 +0530

    footer change

--- application/front/views/business_profile/business_profile_manage_post.php
+++ application/front/views/business_profile/business_profile_manage_post.php
@@ -412,8 +412,8 @@ if (!file_exists($this->config->item('bus_profile_thumb_upload_path') . $busines
 
         <script src="<?php echo base_url('js/jquery.wallform.js?ver='.time()); ?>"></script>
         <script src="<?php  echo base_url('js/jquery-ui.min.js?ver='.time()); ?>"></script>
-<!--        <script src="<?php // echo base_url('js/demo/jquery-1.9.1.js?ver='.time()); ?>"></script> 
-        <script src="<?php // echo base_url('js/demo/jquery-ui-1.9.1.js?ver='.time()); ?>"></script> -->
+        <script src="<?php  echo base_url('js/demo/jquery-1.9.1.js?ver='.time()); ?>"></script> 
+        <script src="<?php  echo base_url('js/demo/jquery-ui-1.9.1.js?ver='.time()); ?>"></script> 
         <script src="<?php echo base_url('assets/js/croppie.js?ver='.time()); ?>"></script>
 
         <script type = "text/javascript" src="<?php echo base_url('js/jquery.form.3.51.js?ver='.time()) ?>"></script> 
