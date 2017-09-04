5510056fb2b94206f23855ff9af381d832ece2e9
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-09-04 10:44:40 +0530

    script for changes for edit post length

--- js/webpage/artistic/dashboard.js
+++ js/webpage/artistic/dashboard.js
@@ -1,4 +1,45 @@
  
+
+ function check_length(my_form)
+            {
+                maxLen = 50;
+             
+
+                // max number of characters allowed
+                if (my_form.my_text.value.length > maxLen) {
+                    // Alert message if maximum limit is reached. 
+                    // If required Alert can be removed. 
+                    var msg = "You have reached your maximum limit of characters allowed";
+                    $("#test-upload-product").prop("readonly", true);
+                    //    alert(msg);
+                   // my_form.text_num.value = maxLen - my_form.my_text.value.length;
+                    $('.biderror .mes').html("<div class='pop_content'>" + msg + "</div>");
+                    $('#bidmodal-limit').modal('show');
+                    // Reached the Maximum length so trim the textarea
+                    my_form.my_text.value = my_form.my_text.value.substring(0, maxLen);
+                } else {
+                    // Maximum length not reached so update the value of my_text counter
+                    my_form.text_num.value = maxLen - my_form.my_text.value.length;
+                }
+            }
+
+function check_lengthedit(abc)
+   { 
+       maxLen = 50;
+       var product_name = document.getElementById("editpostname" +abc).value;
+       if (product_name.length > maxLen) { 
+           text_num = maxLen - product_name.length;
+           var msg = "You have reached your maximum limit of characters allowed";
+           $("#editpostname" + abc).prop("readonly", true);
+           $('#postedit .mes').html("<div class='pop_content'>" + msg + "</div>");
+           $('#postedit').modal('show');
+           var substrval = product_name.substring(0, maxLen);
+           $('#editpostname' + abc).val(substrval);
+       } else { 
+           text_num = maxLen - product_name.length;
+           document.getElementById("text_num_" + abc).value = text_num;
+       }
+   }
 // photos video audion pdf fatch using ajax
 
  $(document).ready(function () {
@@ -194,46 +235,7 @@ var b = document.getElementById('test-upload-des').value;
 
 
 
-function check_length(my_form)
-            {
-                maxLen = 50;
-             
 
-                // max number of characters allowed
-                if (my_form.my_text.value.length > maxLen) {
-                    // Alert message if maximum limit is reached. 
-                    // If required Alert can be removed. 
-                    var msg = "You have reached your maximum limit of characters allowed";
-                    $("#test-upload-product").prop("readonly", true);
-                    //    alert(msg);
-                   // my_form.text_num.value = maxLen - my_form.my_text.value.length;
-                    $('.biderror .mes').html("<div class='pop_content'>" + msg + "</div>");
-                    $('#bidmodal-limit').modal('show');
-                    // Reached the Maximum length so trim the textarea
-                    my_form.my_text.value = my_form.my_text.value.substring(0, maxLen);
-                } else {
-                    // Maximum length not reached so update the value of my_text counter
-                    my_form.text_num.value = maxLen - my_form.my_text.value.length;
-                }
-            }
-
-function check_lengthedit(abc)
-   { 
-       maxLen = 50;
-       var product_name = document.getElementById("editpostname" +abc).value;
-       if (product_name.length > maxLen) { 
-           text_num = maxLen - product_name.length;
-           var msg = "You have reached your maximum limit of characters allowed";
-           $("#editpostname" + abc).prop("readonly", true);
-           $('#postedit .mes').html("<div class='pop_content'>" + msg + "</div>");
-           $('#postedit').modal('show');
-           var substrval = product_name.substring(0, maxLen);
-           $('#editpostname' + abc).val(substrval);
-       } else { 
-           text_num = maxLen - product_name.length;
-           document.getElementById("text_num_" + abc).value = text_num;
-       }
-   }
 
    
             var modal = document.getElementById('myModal3');
