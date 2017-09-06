b9b9585df97e3ada744e7bcbef81b207ebe509d2
khyati@123456 <khyati.aileensoul@gmail.com>
2017-09-06 10:56:56 +0530

    recruiter changes in controller

--- application/front/views/recruiter/saved_candidate.php
+++ application/front/views/recruiter/saved_candidate.php
@@ -5,6 +5,7 @@
         <?php echo $head; ?> 
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/1.10.3.jquery-ui.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/croppie.css?ver='.time()); ?>" />
         <link rel="stylesheet" href="<?php echo base_url() ?>css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/test.css'); ?>">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/profiles/recruiter/recruiter.css'); ?>">
