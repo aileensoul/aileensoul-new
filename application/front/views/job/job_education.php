bfa632ad71e9d8a099e302526fa5302d662433fc
aarati-aileensoul <aarati.aileensoul@gmail.com>
2017-09-14 19:42:00 +0530

    aarati changes

--- application/front/views/job/job_education.php
+++ application/front/views/job/job_education.php
@@ -824,7 +824,7 @@
                                           <h6>
                                              Grade :
                                           </h6>
-                                          <input type="text" name="grade[]" id="grade1" class="grade" placeholder="Enter Grade" value="<?php
+                                          <input type="text" name="grade[]" id="grade1" class="grade" placeholder="Ex : (A+,B+,C+,D+)" style="text-transform: uppercase;"  value="<?php
                                              if ($grade1) {
                                                  echo $grade1;
                                              }
