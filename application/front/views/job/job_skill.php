ba1777fb04b1d2599bdd1ec261f2ab96197ab0a4
aarati-aileensoul <aarati.aileensoul@gmail.com>
2017-09-18 14:13:42 +0530

    aarati changes

--- application/front/views/job/job_skill.php
+++ application/front/views/job/job_skill.php
@@ -100,7 +100,7 @@
       <script type="text/javascript" src="<?php echo base_url('js/jquery.validate.min.js?ver='.time()) ?>"></script>
       <script type="text/javascript" src="<?php echo base_url('js/jquery.validate.js?ver='.time()); ?>"></script>
       <script type="text/javascript" src="<?php echo base_url('js/additional-methods1.15.0.min.js?ver='.time()); ?>"></script>
-      <script src="<?php //echo base_url('js/bootstrap.min.js?ver='.time()); ?>"></script>
+      <script src="<?php echo base_url('js/bootstrap.min.js?ver='.time()); ?>"></script>
       <script>
          var base_url = '<?php echo base_url(); ?>';
       </script>
