f43af0b3c11b46f10a4972d46101e78fa8510f0a
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-09-20 11:27:25 +0530

    js and header beautify

--- application/front/views/footer.php
+++ application/front/views/footer.php
@@ -1,37 +1,35 @@
-
 <!-- CLOSE ALL DROPEDOWN WHEN CLICK TO BODY OR OTHER DROPDOWN START -->
- <script type="text/javascript">
-$(document).ready(function(){
-    $('.dropbtn_common').on('click',function(){
-        $('.dropbtn_common').not(this).next().removeClass('show');
-        $(this).next().toggleClass('show');
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.dropbtn_common').on('click', function() {
+            $('.dropbtn_common').not(this).next().removeClass('show');
+            $(this).next().toggleClass('show');
+        });
+        $(document).on('click', function(e) {
+            if (!$(e.target).closest('.dropbtn_common').length)
+                $('.dropbtn_common').next().removeClass('show');
+        });
+        $(document).on('keydown', function(e) {
+            if (e.keyCode === 27) {
+                $('.dropbtn_common').next().removeClass('show');
+            }
+        });
     });
-    $(document).on('click',function(e){
-        if(!$(e.target).closest('.dropbtn_common').length)
-            $('.dropbtn_common').next().removeClass('show');
-    });  
-    $(document).on('keydown', function (e) {
-        if (e.keyCode === 27) 
-        {
-            $('.dropbtn_common').next().removeClass('show');
-        }
-    });  
-});
 </script>
 <!-- CLOSE ALL DROPEDOWN WHEN CLICK TO BODY OR OTHER DROPDOWN END -->
 <script type="text/javascript">
-
-$.fn.preload = function (fn) {
-    var len = this.length, i = 0;
-    return this.each(function () {
-        var tmp = new Image, self = this;
-        if (fn) tmp.onload = function () {
-            fn.call(self, 100 * ++i / len, i === len);
-        };
-        tmp.src = this.src;
-    });
-};
-
+    $.fn.preload = function(fn) {
+        var len = this.length,
+            i = 0;
+        return this.each(function() {
+            var tmp = new Image,
+                self = this;
+            if (fn) tmp.onload = function() {
+                fn.call(self, 100 * ++i / len, i === len);
+            };
+            tmp.src = this.src;
+        });
+    };
 </script>
 <!-- IMAGE PRELOADER SCRIPT -->
 <!-- script for update all read notification start-->
@@ -40,33 +38,34 @@ $.fn.preload = function (fn) {
         getNotification();
         notheader();
     }
+
     function getNotification() {
         // first click alert('here'); 
         $.ajax({
             url: "<?php echo base_url(); ?>notification/update_notification",
             type: "POST",
-            success: function (data) {
+            success: function(data) {
                 data = JSON.parse(data);
             }
         });
     }
-    function notheader()
-    {
+
+    function notheader() {
 
         $.ajax({
             type: 'POST',
             url: '<?php echo base_url() . "notification/not_header" ?>',
             dataType: 'json',
             data: '',
-            beforeSend: function () {
-            
+            beforeSend: function() {
+
                 $('#gen_not_loader').show();
-           },
-        
-        complete: function () {
-            $('#gen_not_loader').hide();
-        },
-            success: function (data) {
+            },
+
+            complete: function() {
+                $('#gen_not_loader').hide();
+            },
+            success: function(data) {
                 //    alert(data);
                 $('.' + 'notification_data_in').html(data.notification);
                 $('#seenot').html(data.seeall);
@@ -77,26 +76,26 @@ $.fn.preload = function (fn) {
 
     }
 
-    jQuery(document).ready(function ($) {
+    jQuery(document).ready(function($) {
         if (screen.width <= 767) {
-            $("ul.left-form-each").on("click", ".init", function () {
+            $("ul.left-form-each").on("click", ".init", function() {
                 $(this).closest("ul").children('li:not(.init)').toggle();
             });
             var allOptions = $("ul").children('li:not(.init)');
-            $("ul.left-form-each").on("click", "li:not(.init)", function () {
+            $("ul.left-form-each").on("click", "li:not(.init)", function() {
                 allOptions.removeClass('selected');
                 $(this).addClass('selected');
                 $("ul.left-form-each").children('.init').html($(this).html());
                 allOptions.toggle();
             });
         }
-        $(function () {
-            $('a[href="#search"]').on('click', function (event) {
+        $(function() {
+            $('a[href="#search"]').on('click', function(event) {
                 event.preventDefault();
                 $('#search').addClass('open');
                 $('#search > form > input[type="search"]').focus();
             });
-            $('#search, #search button.close').on('click keyup', function (event) {
+            $('#search, #search button.close').on('click keyup', function(event) {
                 if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
                     $(this).removeClass('open');
                 }
@@ -104,74 +103,72 @@ $.fn.preload = function (fn) {
         });
     });
 
-    $(document).ready(function () {
+    $(document).ready(function() {
         // MAIN NOTIFICATION
         waitForMsg();
         $menuLeft = $('.pushmenu-left');
         $nav_list = $('#nav_list');
-        $nav_list.click(function () {
+        $nav_list.click(function() {
             $(this).toggleClass('active');
             $('.pushmenu-push').toggleClass('pushmenu-push-toright');
             $menuLeft.toggleClass('pushmenu-open');
         });
         // CONTACT PERSON COUNT
-       // waitForMsg_contact();
+        // waitForMsg_contact();
         $menuLeft = $('.pushmenu-left');
         $nav_list = $('#nav_list');
-        $nav_list.click(function () {
+        $nav_list.click(function() {
             $(this).toggleClass('active');
             $('.pushmenu-push').toggleClass('pushmenu-push-toright');
             $menuLeft.toggleClass('pushmenu-open');
         });
-     
+
     });
 
-    function addmsg(type, msg)
-    {
-        if (msg == 0)
-        {
+    function addmsg(type, msg) {
+        if (msg == 0) {
             $("#notification_count").html('');
-            $('#notification_count').css({"background-color": " ", "padding": "5px 6px"});
+            $('#notification_count').css({
+                "background-color": " ",
+                "padding": "5px 6px"
+            });
             $('#notificationLink').removeClass('notification_available');
-        } else
-        { 
+        } else {
             $('#notification_count').html(msg);
-            $('#notification_count').css({"background-color": "#FF4500", "padding": "5px 6px"});
+            $('#notification_count').css({
+                "background-color": "#FF4500",
+                "padding": "5px 6px"
+            });
             $('#notificationLink').addClass('notification_available');
-         //   document.getElementById('message_count').style.display = "none";
+            //   document.getElementById('message_count').style.display = "none";
             document.getElementById('notification_count').style.display = 'block';
         }
     }
-    function waitForMsg()
-    {
+
+    function waitForMsg() {
         $.ajax({
             type: "GET",
             url: "<?php echo base_url(); ?>notification/select_notification",
             async: true,
             cache: false,
             timeout: 50000,
-            
-            success: function (data) {
+
+            success: function(data) {
                 addmsg("new", data);
                 setTimeout(
-                        waitForMsg,
-                        10000
-                        );
+                    waitForMsg,
+                    10000
+                );
             },
         });
     }
-    
-    // CLICK ON ESCAPE NOTIFICATION & MESSAGE DROP DOWN CLOSE END
-
- 
+  // CLICK ON ESCAPE NOTIFICATION & MESSAGE DROP DOWN CLOSE END
 </script>
-
 <script type="text/javascript">
-    $('#InboxLink').on('click', function () {
+    $('#InboxLink').on('click', function() {
         document.getElementById('message_count').style.display = "none";
     });
 </script>
-
 <!-- footer end -->
 <!--SCRIPT USE FOR NOTIFICATION SCROLLBAR-->
 <script type="text/javascript" src="<?php echo base_url('js/scrollbar/jquery.mCustomScrollbar.concat.min.js'); ?>"></script>
