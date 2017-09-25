f12a826b401594562bdf9efe2e89efab7ba8fc4c
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-09-25 16:58:12 +0530

    include separate

--- application/front/controllers/artistic_include.php
+++ application/front/controllers/artistic_include.php
@@ -6,63 +6,6 @@ $this->data['userdata'] = $this->common->select_data_by_condition('user', $conti
 // artistics detail
 $contition_array = array('user_id' => $userid, 'is_delete' => '0', 'status' => '1');
 $this->data['artdata'] = $this->common->select_data_by_condition('art_reg', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
-//for message notification start
-$contition_array = array('notification.not_type' => 8, 'notification.not_from' => 3, 'notification.not_to_id' => $userid, 'created_date BETWEEN DATE_SUB(NOW(), INTERVAL 2 MONTH) AND NOW()');
-$join_str = array(array(
-        'join_type' => '',
-        'table' => 'follow',
-        'join_table_id' => 'notification.not_product_id',
-        'from_table_id' => 'follow.follow_id'),
-    array(
-        'join_type' => '',
-        'table' => 'user',
-        'join_table_id' => 'notification.not_from_id',
-        'from_table_id' => 'user.user_id')
-);
-$data = array('notification.*', ' follow.*', ' user.user_id', 'user.first_name', 'user.user_image', 'user.last_name');
-$this->data['artfollow'] = $this->common->select_data_by_condition('notification', $contition_array, $data, $sortby = 'follow_id', $orderby = 'desc', $limit = '', $offset = '', $join_str, $groupby = 'not_from_id');
-$contition_array = array('notification.not_type' => 6, 'notification.not_from' => 3, 'notification.not_to_id' => $userid, 'created_date BETWEEN DATE_SUB(NOW(), INTERVAL 2 MONTH) AND NOW()');
-$join_str = array(array(
-        'join_type' => '',
-        'table' => 'artistic_post_comment',
-        'join_table_id' => 'notification.not_product_id',
-        'from_table_id' => 'artistic_post_comment.artistic_post_comment_id'),
-    array(
-        'join_type' => '',
-        'table' => 'user',
-        'join_table_id' => 'notification.not_from_id',
-        'from_table_id' => 'user.user_id')
-);
-$data = array('notification.*', ' artistic_post_comment.*', ' user.user_id', 'user.first_name', 'user.user_image', 'user.last_name');
-$this->data['artcommnet'] = $this->common->select_data_by_condition('notification', $contition_array, $data, $sortby = 'artistic_post_comment_id', $orderby = 'desc', $limit = '', $offset = '', $join_str, $groupby = 'not_from_id');
-$contition_array = array('notification.not_type' => 5, 'notification.not_from' => 3, 'notification.not_to_id' => $userid, 'created_date BETWEEN DATE_SUB(NOW(), INTERVAL 2 MONTH) AND NOW()');
-$join_str = array(array(
-        'join_type' => '',
-        'table' => 'art_post',
-        'join_table_id' => 'notification.not_product_id',
-        'from_table_id' => 'art_post.art_post_id'),
-    array(
-        'join_type' => '',
-        'table' => 'user',
-        'join_table_id' => 'notification.not_from_id',
-        'from_table_id' => 'user.user_id')
-);
-$data = array('notification.*', 'art_post.*', ' user.user_id', 'user.first_name', 'user.user_image', 'user.last_name');
-$this->data['artlike'] = $this->common->select_data_by_condition('notification', $contition_array, $data, $sortby = 'art_post_id', $orderby = 'desc', $limit = '', $offset = '', $join_str, $groupby = 'not_from_id');
-$contition_array = array('notification.not_type' => 8, 'notification.not_from' => 6, 'notification.not_to_id' => $userid, 'created_date BETWEEN DATE_SUB(NOW(), INTERVAL 2 MONTH) AND NOW()');
-$join_str = array(array(
-        'join_type' => '',
-        'table' => 'follow',
-        'join_table_id' => 'notification.not_product_id',
-        'from_table_id' => 'follow.follow_id'),
-    array(
-        'join_type' => '',
-        'table' => 'user',
-        'join_table_id' => 'notification.not_from_id',
-        'from_table_id' => 'user.user_id')
-);
-$data = array('notification.*', 'follow.*', 'user.user_id', 'user.first_name', 'user.user_image', 'user.last_name');
-$this->data['busifollow'] = $this->common->select_data_by_condition('notification', $contition_array, $data, $sortby = 'follow_id', $orderby = 'desc', $limit = '', $offset = '', $join_str, $groupby = 'not_from_id');
 $this->data['head'] = $this->load->view('head', $this->data, true);
 $this->data['header'] = $this->load->view('header', $this->data, true);
 $this->data['footer'] = $this->load->view('footer', $this->data, true);
@@ -81,9 +24,7 @@ foreach ($followerdata as $followkey) {
 $this->data['flucount'] = $flucount = count($countlu);
 $contition_array = array('follow_from' => $artregid, 'follow_status' => '1', 'follow_type' => '1');
 $followingdata = $this->data['followingdata'] = $this->common->select_data_by_condition('follow', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
-
 foreach ($followingdata as $followkey) {
-
     $contition_array = array('art_id' => $followkey['follow_to'], 'status' => '1');
     $artaval = $this->common->select_data_by_condition('art_reg', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
     if ($artaval) {
@@ -93,5 +34,4 @@ foreach ($followingdata as $followkey) {
 }
 $this->data['countfr'] = $countfr = count($countlfu);
 $this->data['art_header2_border'] = $this->load->view('artistic/art_header2_border', $this->data, true);
-
 ?>
