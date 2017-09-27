98c36f04a589ba093ff35928c4a82688f39b1cfb
harshad-aileensoul <harshad.aileensoul@gmail.com>
2017-09-27 15:59:52 -0400

    style changes

--- application/front/views/business_profile/business_details.php
+++ application/front/views/business_profile/business_details.php
@@ -353,14 +353,14 @@
 
         </style>
     </head>
-    <body class="page-container-bg-solid page-boxed pushmenu-push">
+    <body class="page-container-bg-solid page-boxed pushmenu-push no-login">
         <header>
             <div class="container">
                 <div class="row">
-                    <div class="col-md-4 col-sm-3">
+                    <div class="col-md-4 col-sm-3 col-xs-4 fw-479">
                         <h2 class="logo"><a href="<?php echo base_url(); ?>">Aileensoul</a></h2>
                     </div>
-                    <div class="col-md-8 col-sm-9">
+                    <div class="col-md-8 col-sm-9 col-xs-8 fw-479">
                         <div class="btn-right pull-right">
                             <a href="javascript:void(0);" onclick="login_profile();" class="btn2">Login</a>
                             <a href="javascript:void(0);" onclick="register_profile();" class="btn3">Creat an account</a>
