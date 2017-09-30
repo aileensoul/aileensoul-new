22fe60946739badf90ef7842e1fa3211366993f5
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-09-30 13:27:32 +0530

    ci validation

--- application/front/views/artistic/art_basic_information.php
+++ application/front/views/artistic/art_basic_information.php
@@ -67,6 +67,7 @@
                                  $lastname = form_error('lastname');
                                  $email =  form_error('email');
                                  $phoneno =  form_error('phoneno');
+                                 
                          		?>
                                <fieldset <?php if($firstname) {  ?> class="error-msg" <?php } ?>>
                                     <label>First Name:<span style="color:red">*</span></label>
