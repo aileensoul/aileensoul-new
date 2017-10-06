7931edf8ad094813c5103146d971a65a1dfe9589
harshad-aileensoul <harshad.aileensoul@gmail.com>
2017-10-06 14:32:03 -0400

    style

--- application/front/views/business_profile/business_profile_post.php
+++ application/front/views/business_profile/business_profile_post.php
@@ -111,12 +111,12 @@
                         </div>
                     </div>
                     <div class="right_middle_side_posrt fixed_right_display animated fadeInRightBig"> 
-                        <script type="text/javascript" language="javascript">
+                        <!--script type="text/javascript" language="javascript">
 						  var aax_size='300x250';
 						  var aax_pubname = 'aileensoul-21';
 						  var aax_src='302';
 						</script>
-						<script type="text/javascript" language="javascript" src="http://c.amazon-adsystem.com/aax2/assoc.js"></script>
+						<script type="text/javascript" language="javascript" src="http://c.amazon-adsystem.com/aax2/assoc.js"></script-->
                     </div>
                 </div>
             </div>
