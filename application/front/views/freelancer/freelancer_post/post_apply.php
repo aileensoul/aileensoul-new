26d48a29a03d7d9206ae3fd51652ea9ed2d7d06f
harshad-aileensoul <harshad.aileensoul@gmail.com>
2017-10-07 20:44:24 -0400

    style

--- application/front/views/freelancer/freelancer_post/post_apply.php
+++ application/front/views/freelancer/freelancer_post/post_apply.php
@@ -161,13 +161,13 @@
 						</div>
 						<div class="tablate-add">
 					
-					<script type="text/javascript" language="javascript">
-					  var aax_size='160x600';
-					  var aax_pubname = 'aileensoul-21';
-					  var aax_src='302';
-					</script>
-					<script type="text/javascript" language="javascript" src="http://c.amazon-adsystem.com/aax2/assoc.js"></script>
-				</div>
+							<script type="text/javascript" language="javascript">
+							  var aax_size='160x600';
+							  var aax_pubname = 'aileensoul-21';
+							  var aax_src='302';
+							</script>
+							<script type="text/javascript" language="javascript" src="http://c.amazon-adsystem.com/aax2/assoc.js"></script>
+						</div>
 					</div>
                 </div>
             </div>
