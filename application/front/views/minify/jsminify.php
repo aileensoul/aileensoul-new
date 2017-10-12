7bfff7e02ff4c3c493f83c9d39a4a3099ea5c5ed
khyati@123456 <khyati.aileensoul@gmail.com>
2017-10-12 12:08:13 +0530

    js changes in bus

--- application/front/views/minify/jsminify.php
+++ application/front/views/minify/jsminify.php
@@ -7,7 +7,29 @@ $bus_info = array(
 );
 $this->minify->js($bus_info);
 echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/common.min.js');
-// BUSINESS_COMMON END 
+
+
+// BUSINESS_AUDIO START   
+$bus_audio = array(
+    'webpage/business-profile/common.js',
+);
+$this->minify->js($bus_audio);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/audio.min.js');
+
+// BUSINESS_CONTACT_PERSON START   
+$bus_contatt_person = array(
+    'webpage/business-profile/common.js',
+);
+$this->minify->js($bus_contatt_person);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/contactperson.min.js');
+
+// BUSINESS_DETAILS START   
+$bus_details = array(
+    'webpage/business-profile/common.js',
+);
+$this->minify->js($bus_details);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/details.min.js');
+
 
 // BUSINESS_INFO START   
 $bus_info = array('webpage/business-profile/information.js',
@@ -26,23 +48,80 @@ $bus_contact = array('webpage/business-profile/contacts.js',
 $this->minify->js($bus_contact);
 echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/contacts.min.js');
 
-// BUSINESS_CONTACT FOOTER   
+// BUSINESS_EDIT_PROFILE START   
+$bus_edit_profile = array('webpage/business-profile/edit_profile.js',
+    '../js_min/webpage/business-profile/common.min.js',
+);
+
+$this->minify->js($bus_edit_profile);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/edit_profile.min.js');
+
+// FOLLOWERS START   
+$followers = array('webpage/business-profile/followers.js',
+    '../js_min/webpage/business-profile/common.min.js',
+);
+
+$this->minify->js($followers);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/followers.min.js');
+
+// FOLLOWING START   
+$following = array('webpage/business-profile/following.js',
+    '../js_min/webpage/business-profile/common.min.js',
+);
+
+$this->minify->js($following);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/following.min.js');
+
+// PDF START   
+$pdf = array('webpage/business-profile/pdf.js',
+    '../js_min/webpage/business-profile/common.min.js',
+);
+
+$this->minify->js($pdf);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/pdf.min.js');
+
+// PHOTO START   
+$photos = array('webpage/business-profile/photos.js',
+    '../js_min/webpage/business-profile/common.min.js',
+);
+
+$this->minify->js($photos);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/photos.min.js');
+
+// BUSINESS PROFILE MANAGE POST 
+$dashboard = array('webpage/business-profile/dashboard.js',
+    '../js_min/webpage/business-profile/common.min.js',
+);
+
+$this->minify->js($dashboard);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/dashboard.min.js');
+
+// BUSINESS PROFILE MANAGE POST 
+$profile_post = array(
+    '../js_min/webpage/business-profile/common.min.js',
+    'webpage/business-profile/home.js',
+);
+
+$this->minify->js($profile_post);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/home.min.js');
+
+// BUSINESS_CONTACT FOOTER CROPPI BOOTSTRAP VALIDATE  
 $bus_contact_footer = array(
     '../js_min/croppie.min.js',
     'bootstrap.min.js',
     'jquery.validate.min.js',
 );
 
-$this->minify->js($bus_contact);
-echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/bus_contacts_footer.min.js');
+$this->minify->js($bus_contact_footer);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/croppie_bootstrap_validate.min.js');
 
 //BOOTSTARP AND VALIDATE MIN   
-$bus_contact_footer = array(
+$bootstrap_valdidate = array(
     'bootstrap.min.js',
     'jquery.validate.min.js',
 );
 
-$this->minify->js($bus_contact);
+$this->minify->js($bootstrap_valdidate);
 echo $this->minify->deploy_js(FALSE, 'bootstrap_validate.min.js');
 
 
