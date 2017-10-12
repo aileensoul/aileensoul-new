b3076b36214d7e4bbd73c84cbab4b87b07589e9c
khyati@123456 <khyati.aileensoul@gmail.com>
2017-10-12 11:37:08 +0530

    js changes

--- application/front/views/minify/jsminify.php
+++ application/front/views/minify/jsminify.php
@@ -15,25 +15,70 @@ $bus_info = array('webpage/business-profile/information.js',
 );
 
 $this->minify->js($bus_info);
-echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/business-info.min.js');
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/information.min.js');
 // BUSINESS_INFO END 
 
 // BUSINESS_CONTACT START   
-$bus_info = array('webpage/business-profile/contacts.js',
+$bus_contact = array('webpage/business-profile/contacts.js',
     '../js_min/webpage/business-profile/common.min.js',
 );
 
-$this->minify->js($bus_info);
+$this->minify->js($bus_contact);
 echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/contacts.min.js');
-// BUSINESS_CONTACT END 
+
+// BUSINESS_CONTACT FOOTER   
+$bus_contact_footer = array(
+    '../js_min/croppie.min.js',
+    'bootstrap.min.js',
+    'jquery.validate.min.js',
+);
+
+$this->minify->js($bus_contact);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/bus_contacts_footer.min.js');
+
+//BOOTSTARP AND VALIDATE MIN   
+$bus_contact_footer = array(
+    'bootstrap.min.js',
+    'jquery.validate.min.js',
+);
+
+$this->minify->js($bus_contact);
+echo $this->minify->deploy_js(FALSE, 'bootstrap_validate.min.js');
+
 
 // BUSINESS_SEARCH_LOGIN START   
-$bus_info = array('webpage/business-profile/contacts.js',
+$bus_search = array('webpage/business-profile/contacts.js',
     '../js_min/webpage/business-profile/common.min.js',
 );
 
-$this->minify->js($bus_info);
+$this->minify->js($bus_search);
 echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/bus_search_login.min.js');
+
+// BUSINESS_SEARCH_LOGIN HEADER START   
+$bus_search_header = array('../dragdrop/js/plugins/sortable.js',
+    'fileinput.js',
+    '../dragdrop/themes/explorer/theme.js',
+);
+
+$this->minify->js($bus_search_header);
+echo $this->minify->deploy_js(FALSE, 'webpage/business-profile/bus_search_login_header.min.js');
+
+// CROPPIE START   
+$croppie = array('croppie.js',
+);
+
+$this->minify->js($croppie);
+echo $this->minify->deploy_js(FALSE, 'croppie.min.js');
+
+// CROPPIE START   
+$croppie = array('croppie.js',
+);
+
+$this->minify->js($croppie);
+echo $this->minify->deploy_js(FALSE, 'croppie.min.js');
+
+
+
 
 
 //$this->minify->js(array('helpers.js', 'jqModal.js'));
