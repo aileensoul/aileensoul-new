ac15222b3acdc2f77a58657f2515e34b40cdaf78
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-10-12 12:10:52 +0530

    fancy box repalce by model in job

--- application/front/views/job/job_reg.php
+++ application/front/views/job/job_reg.php
@@ -98,6 +98,20 @@
    </section>
    <!-- END CONTAINER -->
 
+    <!-- Bid-modal  -->
+      <div class="modal fade message-box biderror" id="bidmodal" role="dialog"  >
+         <div class="modal-dialog modal-lm" >
+            <div class="modal-content">
+               <button type="button" class="modal-close" data-dismiss="modal">&times;</button>       
+               <div class="modal-body">
+                  <span class="mes"></span>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- Model Popup Close -->
+      
+
 <footer>        
 <?php echo $footer;  ?>
 </footer>
