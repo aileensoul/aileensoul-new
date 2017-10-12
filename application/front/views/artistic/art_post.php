45be3a86e629c66bfa26fcd779ebf9cea3423a62
harshad-aileensoul <harshad.aileensoul@gmail.com>
2017-10-12 18:51:23 -0400

    style

--- application/front/views/artistic/art_post.php
+++ application/front/views/artistic/art_post.php
@@ -158,14 +158,14 @@ if(count($usercount) != count($followcount) + count($crosscount)){?>
      
 					<div class="fw text-center">
                         <script type="text/javascript">
-  ( function() {
-    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
-    var unit = {"calltype":"async[2]","publisher":"Aileensoul","width":300,"height":250,"sid":"Chitika Default"};
-    var placement_id = window.CHITIKA.units.length;
-    window.CHITIKA.units.push(unit);
-    document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
-}());
-</script>
+						  ( function() {
+							if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
+							var unit = {"calltype":"async[2]","publisher":"Aileensoul","width":300,"height":250,"sid":"Chitika Default"};
+							var placement_id = window.CHITIKA.units.length;
+							window.CHITIKA.units.push(unit);
+							document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
+						}());
+						</script>
 <script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
 					</div>
 					<div class="fw pt20" style="text-align:center;">
