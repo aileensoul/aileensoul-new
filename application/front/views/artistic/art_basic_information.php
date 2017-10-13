6ddf6add234c9b44cded398ef487f4ae7d32cb3b
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-10-13 10:33:01 +0530

    basic information query

--- application/front/views/artistic/art_basic_information.php
+++ application/front/views/artistic/art_basic_information.php
@@ -22,9 +22,7 @@
            <div class="row">
              <div class="col-md-3 col-sm-4"></div>
              <?php 
-             $userid = $this->session->userdata('aileenuser');
-             $contition_array = array('user_id' => $userid, 'status' => '1');
-             $artdata = $this->common->select_data_by_condition('art_reg', $contition_array, $data = '*', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');           
+                       
              if($artdata[0]['art_step'] == 4){ ?>
  <div class="col-md-6 col-sm-8"><h3>You are updating your Artistic Profile.</h3></div>
                 <?php }else{
