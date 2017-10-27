51cd1a242219a5e597759738a988abc1e96dca55
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-10-27 15:33:57 +0530

    art category

--- assets/js/webpage/artistic/art_information.js
+++ assets/js/webpage/artistic/art_information.js
@@ -63,69 +63,97 @@ $.validator.addMethod("regx", function (value, element, regexpr) {
                    });
 
 
+// other category input open start
 
-$(function() {
-        function split( val ) {
-            return val.split( /,\s*/ );
+$('#skills').change(function other_category(){
+        var e = document.getElementById("skills");
+        var strUser = e.options[e.selectedIndex].value;
+        if(strUser == 17){
+            document.getElementById('other_category').style.display = "block";
+        }else{
+            document.getElementById('other_category').style.display = "none"; 
         }
-        function extractLast( term ) { 
-            return split( term ).pop();
-        }
-        
-        $( "#skills2" ).bind( "keydown", function( event ) {
-            if ( event.keyCode === $.ui.keyCode.TAB &&
-                $( this ).autocomplete( "instance" ).menu.active ) {
-                event.preventDefault();
-            }
-        })
-        .autocomplete({
-            minLength: 2,
-            source: function( request, response ) { 
-
-                //url: base_url + "general/get_artskill",
-
-                // delegate back to autocomplete, but extract the last term
-                $.getJSON(base_url + "general/get_artskill", { term : extractLast( request.term )},response);
-            },
-            focus: function() {
-                // prevent value inserted on focus
-                return false;
-            },
-            select: function( event, ui ) {
-               
-                 var text =this.value;
-                var terms = split( this.value );
-                 
-                text = text == null || text == undefined ? "" : text;
-                var checked = (text.indexOf(ui.item.value + ', ') > -1 ? 'checked' : '');
-               if (checked == 'checked') {
-      
-                    terms.push( ui.item.value );
-                    this.value = terms.split( ", " );
-               }//if end
-
-              else {
-                if(terms.length <= 10) {
-                    // remove the current input
-                    terms.pop();
-                    // add the selected item
-                    terms.push( ui.item.value );
-                    // add placeholder to get the comma-and-space at the end
-                    terms.push( "" );
-                    this.value = terms.join( ", " );
-                    return false;
-                }else{
-                    var last = terms.pop();
-                    $(this).val(this.value.substr(0, this.value.length - last.length - 2)); // removes text from input
-                    $(this).effect("highlight", {}, 1000);
-                    $(this).attr("style","border: solid 1px red;");
-                    return false;
-                }
-             }//else end
-            }
- 
-        });
     });
+
+function removevalidation(){
+
+   $("#othercategory").removeClass("othercategory_require");
+   $('#othercategory_error').remove();
+}
+function validation_other(){ 
+
+    var other_category = document.getElementById("othercategory").value;
+    var category_trim = other_category.trim();
+   if(category_trim == ''){
+     $("#othercategory").addClass("othercategory_require");
+     $('<span class="error" id="othercategory_error" style="float: right;color: red; font-size: 13px;">Other art category required. </span>').insertAfter('#othercategory');
+      return false;
+      event.prevent();
+       } 
+}
+
+// $(function() {
+//         function split( val ) {
+//             return val.split( /,\s*/ );
+//         }
+//         function extractLast( term ) { 
+//             return split( term ).pop();
+//         }
+        
+//         $( "#skills2" ).bind( "keydown", function( event ) {
+//             if ( event.keyCode === $.ui.keyCode.TAB &&
+//                 $( this ).autocomplete( "instance" ).menu.active ) {
+//                 event.preventDefault();
+//             }
+//         })
+//         .autocomplete({
+//             minLength: 2,
+//             source: function( request, response ) { 
+
+//                 //url: base_url + "general/get_artskill",
+
+//                 // delegate back to autocomplete, but extract the last term
+//                 $.getJSON(base_url + "general/get_artskill", { term : extractLast( request.term )},response);
+//             },
+//             focus: function() {
+//                 // prevent value inserted on focus
+//                 return false;
+//             },
+//             select: function( event, ui ) {
+               
+//                  var text =this.value;
+//                 var terms = split( this.value );
+                 
+//                 text = text == null || text == undefined ? "" : text;
+//                 var checked = (text.indexOf(ui.item.value + ', ') > -1 ? 'checked' : '');
+//                if (checked == 'checked') {
+      
+//                     terms.push( ui.item.value );
+//                     this.value = terms.split( ", " );
+//                }//if end
+
+//               else {
+//                 if(terms.length <= 10) {
+//                     // remove the current input
+//                     terms.pop();
+//                     // add the selected item
+//                     terms.push( ui.item.value );
+//                     // add placeholder to get the comma-and-space at the end
+//                     terms.push( "" );
+//                     this.value = terms.join( ", " );
+//                     return false;
+//                 }else{
+//                     var last = terms.pop();
+//                     $(this).val(this.value.substr(0, this.value.length - last.length - 2)); // removes text from input
+//                     $(this).effect("highlight", {}, 1000);
+//                     $(this).attr("style","border: solid 1px red;");
+//                     return false;
+//                 }
+//              }//else end
+//             }
+ 
+//         });
+//     });
 
 
 jQuery(document).ready(function($) {  
