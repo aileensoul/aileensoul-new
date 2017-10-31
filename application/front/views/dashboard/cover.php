deebe23fc90afd8817d77ed57960ca8852f438d8
dhaval-aileensoul <dhavalshah.aileensoul43@gmail.com>
2017-10-31 18:12:20 +0530

    Set the tooltip

--- application/front/views/dashboard/cover.php
+++ application/front/views/dashboard/cover.php
@@ -882,13 +882,13 @@ if (!file_exists($this->config->item('user_thumb_upload_path') . $image_ori)) {
                         <span class="mes">
                             <div id="popup-form">
                              <form id ="userimage" name ="userimage" class ="clearfix" enctype="multipart/form-data" method="post">
-                               <div class="">
+                               <div class="col-md-5">
 
                                 <div class="user_profile"></div>
 
                                     <input type="file" name="profilepic" accept="image/gif, image/jpeg, image/png" id="upload-one">
                                 </div>
-                                    <div class="pb10 text-center">
+                                    <div class="col-md-7 text-center">
                                         <div id="upload-demo-one" style=""></div>
                                     </div>
                                 <input type="submit"  class="upload-result-one" name="profilepicsubmit" id="profilepicsubmit" value="Save">
