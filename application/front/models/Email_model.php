109c03f66356a3aeceebadd184ae9215a38ceebd
DESKTOP-FNQCPB7\A <ankit.aileensoul@gmail.com>
2017-10-31 19:43:47 +0530

    Business Updated Work

--- application/front/models/Email_model.php
+++ application/front/models/Email_model.php
@@ -15,6 +15,7 @@ class Email_model extends CI_Model {
         $email_html = '';
         $email_html .= '<!DOCTYPE html><html><head><title>Aileensoul Notification Mail</title>
             <style>
+            body{font-family:arial;}
             p{margin:0;}h3{margin:0;}
             .btn{
 		background: -moz-linear-gradient(96deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* ff3.6+ */
