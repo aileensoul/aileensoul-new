f5df740d4d8bd5d8aa5a83572fb0b328457e13f9
DESKTOP-FNQCPB7\A <ankit.aileensoul@gmail.com>
2017-10-31 17:17:00 +0530

    business profile include

--- application/front/models/Email_model.php
+++ application/front/models/Email_model.php
@@ -9,9 +9,59 @@ class Email_model extends CI_Model {
         parent::__construct();
     }
 
-    function send_email() {
+    function send_email($subject='', $templ='', $to_email='') {
         $this->load->library('email');
-
+        
+        $email_html = '';
+        $email_html .= '<!DOCTYPE html><html><head><title>Aileensoul Notification Mail</title>
+            <style>
+            p{margin:0;}h3{margin:0;}
+            .btn{
+		background: -moz-linear-gradient(96deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* ff3.6+ */
+		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #3bb0ac), color-stop(56%, #1b8ab9), color-stop(100%, #1b8ab9)); /* safari4+,chrome */
+		background: -webkit-linear-gradient(96deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* safari5.1+,chrome10+ */
+		background: -o-linear-gradient(96deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* opera 11.10+ */
+		background: -ms-linear-gradient(96deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* ie10+ */
+		background: linear-gradient(354deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* w3c */
+		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr="#3bb0ac", endColorstr="#1b8ab9",GradientType=0 ); /* ie6-9 */
+		font-size:16px;
+		color:#fff;
+		padding:10px 25px;
+		text-decoration:none;
+	}
+	.btn:hover{/*opacity:0.8;*/}
+</style></head>
+<body>
+	<div style="max-width:600px; margin:0 auto; background:#f4f4f4; padding:30px;">
+		<table width="100%" style="background:#fff" cellpadding="0" cellspacing="0">
+			<tr><td style="border-bottom:1px solid #ddd;">
+                        <table width="100%" cellpadding="0" cellspacing="0">
+						<tr><td style="text-align:center"><h2>
+						<a style="color:#1b8ab9; text-decoration:none; font-size:23px;" href="https://www.aileensoul.com/" target="_blank"><img src="https://www.aileensoul.com/assets/images/favicon.png" alt="Aileensoul" style="   vertical-align: middle;" /> <span class="sitename">Aileensoul</span></a>
+						</h2></td></tr></table>
+			</td></tr>
+			<tr><td style="border-bottom:1px solid #ddd;">
+			<table width="100%" cellpadding="0" cellspacing="0">
+                            <tr><td style="padding:10px 0 30px; font-size:15px;">'. $templ .'</td></tr>
+                        </table>
+			</td></tr>
+			<tr><td style="padding:25px 0px;">
+                        <table width="100%" cellpadding="0" cellspacing="0">
+                            <tr><td style="text-align:center; padding:0 10px;" width="20%"><img src="https://www.aileensoul.com/assets/img/m1.png"><h3 style="font-size:13px;">Job Profile</h3><p style="font-size:11px;">Find best job options and connect with recruiters.</p></td>
+				<td style="text-align:center; padding:0 10px;" width="20%"><img src="https://www.aileensoul.com/assets/img/m2.png"><h3 style="font-size:13px;">Recruiter Profile</h3><p style="font-size:11px;">Hire quality employees here.</p></td>
+				<td style="text-align:center; padding:0 10px;" width="20%"><img src="https://www.aileensoul.com/assets/img/m3.png"><h3 style="font-size:13px; ">Freelance Profile</h3><p style="font-size:11px;">Hire freelancers and also find freelance work.</p></td>
+                                <td style="text-align:center; padding:0 10px;" width="20%"><img src="https://www.aileensoul.com/assets/img/m4.png"><h3 style="font-size:13px;">Business Profile</h3><p style="font-size:11px;">Grow your business network.</p></td>
+				<td style="text-align:center; padding:0 10px;" width="20%"><img src="https://www.aileensoul.com/assets/img/m5.png"><h3 style="font-size:13px;">Artistic Profile</h3><p style="font-size:11px;">Show your art & talent to the world.</p></td>
+                            </tr>
+			</table>
+			</td></tr>
+		</table>
+		<table width="100%" cellpadding="0" cellspacing="0">
+			<tr><td style="text-align:center; padding:10px 0;"><a style="color:#505050; padding:5px 15px; text-decoration:none;" href="#">Unsubscribe</a>|<a style="color:#505050; padding:5px 15px; text-decoration:none;" href="#">Help</a></td></tr>
+		</table>
+	</div></body></html>';
+        
+        
         $config['protocol'] = PROTOCOL;
         $config['smtp_host'] = SMTP_HOST;
         $config['smtp_port'] = SMTP_PORT;
@@ -22,11 +72,11 @@ class Email_model extends CI_Model {
         $config['newline'] = NEWLINE;
         
         $this->email->from('noreply@aileensoul.com', 'Aileensoul');
-
         $this->email->to($to_email);
+        $this->email->bcc('ankit.aileensoul@gmail.com');
         //$this->email->reply_to('no-replay@aileensoul.com', 'Explendid Videos');
         $this->email->subject($subject);
-        $this->email->message($mail_html);
+        $this->email->message($email_html);
         $this->email->set_mailtype("html");
         $this->email->send();
 
