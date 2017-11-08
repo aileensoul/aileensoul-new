ce113fb7ff7881651d61e4df4b9851183f5994fd
dhaval-aileensoul <dhavalshah.aileensoul43@gmail.com>
2017-11-08 15:47:31 +0530

    Set the edit profile in freelancer

--- application/front/views/freelancer/freelancer_post/freelancer_post_avability.php
+++ application/front/views/freelancer/freelancer_post/freelancer_post_avability.php
@@ -78,7 +78,7 @@
                                     $job_type = form_error('job_type');
                                     $work_hour = form_error('work_hour');
                                     ?>
-                                    <fieldset class="col-md-5" <?php if ($inweek) { ?> class="error-msg" <?php } ?>>
+                                    <fieldset class="col-md-6" <?php if ($inweek) { ?> class="error-msg" <?php } ?>>
 
                                        
                                        <?php     if ($livepostid) { ?>
