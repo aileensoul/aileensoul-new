c687d43a398121d59ac8e1ed1588a369e7b189ce
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-11-08 11:39:53 +0530

    bugs

--- application/front/views/artist/artistic_common.php
+++ application/front/views/artist/artistic_common.php
@@ -34,13 +34,19 @@
                 $segment3 = explode('-', $this->uri->segment(3));
                 $slugdata = array_reverse($segment3);
                 $regid = $slugdata[0];
-                              
+            
                 $regslug = $this->uri->segment(3);
+
                 if(is_numeric($regid)) {  
 
                 $userid = $this->db->select('user_id')->get_where('art_reg', array('art_id' => $regid))->row()->user_id;
                }else{
+
+                  if($regslug){
                 $userid = $this->db->select('user_id')->get_where('art_reg', array('slug' => $regslug))->row()->user_id;
+                  }else{
+                    $userid = $this->session->userdata('aileenuser');
+                  }
                }
 
                 $contition_array = array('user_id' => $userid, 'is_delete' => '0', 'status' => '1');
