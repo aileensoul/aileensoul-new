03a5cd128c96d2325984bf0a0f06f9d61c926211
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-11-09 18:59:53 +0530

    bugs

--- admin/application/admin/controllers/goverment.php
+++ admin/application/admin/controllers/goverment.php
@@ -204,9 +204,62 @@ public function delete_category()
                 $this->session->flashdata('error', 'Sorry!! Your data not inserted');
                 redirect('goverment/edit_gov_category/'.$id);
             }
+           
+}
+
+public function add_gov_post() 
+ {
+
+        $this->data['title'] = 'Goverment Job Post| Aileensoul';
+        $this->data['module_name'] = 'Goverment Job Post';
+        $this->data['section_title'] = 'Goverment Job Post';
+
+         $contition_array = array('status' => '1', 'is_delete' => '0');
+         $this->data['job_category'] = $this->common->select_data_by_condition('gov_category', $contition_array, $data = 'id,name', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');  
+
+        $this->load->view('goverment/add_gov_post', $this->data);
+}
+
+
+
+public function add_gov_post_insert() 
+ {
+        //echo "<pre>"; print_r($this->input->post()); die();
+
+       $date = $this->input->post('selday');
+        $month = $this->input->post('selmonth');
+        $year = $this->input->post('selyear');
+        $last_date = $year . '-' . $month . '-' . $date;
+
+
+
+         $data = array(
+                'title' => $this->input->post('post_title'),
+                'category_id' => $this->input->post('category'),
+                'sector' => $this->input->post('gov_sector'),
+                'eligibility' => $this->input->post('gov_elg'),
+                'last_date' => $last_date,
+                'description' => $this->input->post('gov_des'),
+                'apply_link' => $this->input->post('gov_link'),
+                'status' => $this->input->post('status'),
+                'created_date' => date('Y-m-d H:i:s', time()),
+                'is_delete' => '0'
+            );
+
+            $insert_id = $this->common->insert_data_getid($data, 'gov_post');
+
+
+            if ($insert_id) {
+                $this->session->set_flashdata('success', 'Job Post inserted successfully');
+                 redirect('goverment/add_gov_post');
+            } else {
+                $this->session->flashdata('error', 'Sorry!! Your data not inserted');
+                redirect('goverment/add_gov_post');
+            }
 
            
 }
+
 
 }
 
