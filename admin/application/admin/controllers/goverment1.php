ae297388355107843bb3da84fc4e096ae29fa683
pallavi-aileensoul <pallavi.alieensoul@gmail.com>
2017-11-10 14:56:35 +0530

    updated work

--- admin/application/admin/controllers/goverment1.php
+++ admin/application/admin/controllers/goverment1.php
@@ -28,26 +28,50 @@ class Goverment extends MY_Controller {
     }
 
 
-    public function edit_gov_post_insert($id) 
+   public function edit_gov_post_insert($id ='') 
  {
-        //echo "<pre>"; print_r($this->input->post()); die();
-         $data = array(
-                'name' => $this->input->post('gov_name'),
+        
+        $date = $this->input->post('selday');
+        $month = $this->input->post('selmonth');
+        $year = $this->input->post('selyear');
+        $last_date = $year . '-' . $month . '-' . $date;
+        
+        
+        $data = array(
+                'title' => $this->input->post('post_title'),
+                'category_id' => $this->input->post('category'),
+                'post_name' => $this->input->post('postname'),
+                'no_vacancies' => $this->input->post('novacan'),
+                'pay_scale' => $this->input->post('payscale'),
+                'job_location' => $this->input->post('jobloc'),
+                'req_exp' => $this->input->post('reqexp'),
+                'sector' => $this->input->post('gov_sector'),
+                'eligibility' => $this->input->post('gov_elg'),
+                'last_date' => $last_date,
+                'description' => $this->input->post('gov_des'),
+                'apply_link' => $this->input->post('gov_link'),
                 'status' => $this->input->post('status'),
-                'modified_date' => date('Y-m-d H:i:s', time()),
+                'created_date' => date('Y-m-d H:i:s', time()),
+                'is_delete' => '0',
+                'modified_date'=> date('Y-m-d H:i:s', time())
             );
+        
+        
+      //  echo "<pre>"; print_r($this->input->post()); die();
+         
 
-            $updatdata = $this->common->update_data($data, 'gov_category', 'id', $id);
+            $updatdata = $this->common->update_data($data, 'gov_post', 'id', $id);
 
             if ($updatdata) {
                 $this->session->set_flashdata('success', 'Category updated successfully');
-                 redirect('goverment/view_gov_category');
+                 redirect('goverment/view_gov_post');
             } else {
                 $this->session->flashdata('error', 'Sorry!! Your data not inserted');
                 redirect('goverment/edit_gov_category/'.$id);
             }
            
 }
+
 
 
 
