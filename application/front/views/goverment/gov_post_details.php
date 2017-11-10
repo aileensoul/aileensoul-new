b95b3014011ae2c4de83088cbab17eaf6e31c2fe
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-11-10 16:42:53 +0530

    goverment

--- application/front/views/goverment/gov_post_details.php
+++ application/front/views/goverment/gov_post_details.php
@@ -1,1 +1,33 @@
-hii
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+        <?php echo $head; ?>  
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/1.10.3.jquery-ui.css?ver='.time()); ?>">
+        
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/artistic.css?ver='.time()); ?>">
+           
+    </head>
+    <body class="page-container-bg-solid page-boxed">
+    <?php echo $header; ?>
+        
+  <?php echo $login_footer ?>
+     <?php echo $footer;  ?>
+
+    </div>
+
+  
+  <script src="<?php echo base_url('assets/js/bootstrap.min.js?ver=' . time()); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.validate.min.js?ver='.time()) ?>"></script>
+
+<script>
+ var base_url = '<?php echo base_url(); ?>';
+var data= <?php echo json_encode($demo); ?>;
+var data1 = <?php echo json_encode($de); ?>;
+var data1 = <?php echo json_encode($city_data); ?>;
+</script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/webpage/artist/search.js?ver='.time()); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/webpage/artist/information.js?ver='.time()); ?>"></script>
+</body>
+</html>
