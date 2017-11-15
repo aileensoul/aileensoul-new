6b50f2861416e4270ef83ddec92dccaea47d9cec
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-11-15 11:51:16 +0530

    Set the css

--- application/front/views/business_profile/business_header2_border.php
+++ application/front/views/business_profile/business_header2_border.php
@@ -330,7 +330,7 @@
     <div class="modal-dialog modal-lm deactive">
         <div class="modal-content">
             <!--<button type="button" class="modal-close" data-dismiss="modal">&times;</button>-->       
-            <button type="button" class="posterror-modal-close" data-dismiss="modal">&times;</button>       
+            <button type="button" class="modal-close" data-dismiss="modal">&times;</button>       
             <div class="modal-body">
                 <span class="mes"></span>
             </div>
