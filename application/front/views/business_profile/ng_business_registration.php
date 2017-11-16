a47ed1f53d0f91a65ee9e65a46189dd790220697
DESKTOP-FNQCPB7\A <ankit.aileensoul@gmail.com>
2017-11-16 17:56:50 +0530

    Business Registration
    Sign Up Update

--- application/front/views/business_profile/ng_business_registration.php
+++ application/front/views/business_profile/ng_business_registration.php
@@ -198,7 +198,7 @@
 <!--                                                                <select name="business_type" ng-model="user.business_type" ng-change="busSelectCheck(this)" id="business_type" tabindex="1">
                                                                     <option ng-option value="" selected="selected">Select Business type</option>
                                                                 <?php foreach ($business_type as $key => $type) { ?>
-                                                                                                                                                                                                                                                                                                                    <option ng-option value="<?php echo $type->type_id; ?>"><?php echo $type->business_name; ?></option>
+                                                                                                                                                                                                                                                                                                                                    <option ng-option value="<?php echo $type->type_id; ?>"><?php echo $type->business_name; ?></option>
                                                                 <?php } ?>
                                                                     <option ng-option value="0" id="busOption">Other</option>    
                                                                 </select>-->
@@ -214,7 +214,7 @@
 <!--                                                                <select name="industriyal" ng-model="user.industriyal" ng-change="indSelectCheck(this)" id="industriyal" tabindex="2">
                                                                     <option ng-option value="" selected="selected">Select Industry type</option>
                                                                 <?php foreach ($category_list as $key => $category) { ?>
-                                                                                                                                                                                                                                                                                                                    <option ng-option value="<?php echo $category->industry_id; ?>"><?php echo $category->industry_name; ?></option>
+                                                                                                                                                                                                                                                                                                                                    <option ng-option value="<?php echo $category->industry_id; ?>"><?php echo $category->industry_name; ?></option>
                                                                 <?php } ?>
                                                                     <option ng-option value="0" id="indOption">Other</option>
                                                                 </select>-->
@@ -589,12 +589,12 @@
                                                     $('.tab-content .tab-pane:nth-child(2)').addClass('active');
                                                     $scope.tab_active(2);
                                                     getContactInformation();
-//                                                    $("li#left-form-each-li-2 a").attr("href", "#contact_information");
-//                                                    $("li#left-form-each-li-2 a").attr("data-toggle", "tab");
-//                                                    $("li#left-form-each-li-2 a").attr("ng-click", "getContactInformation(); tab_active(2);");
-                                                      $("li#left-form-each-li-2 a").attr("ng-click", "getContactInformation(); tab_active(2);");
-                                                }
-                                                  else {
+                                                    $("li#left-form-each-li-2 a").attr({
+                                                        href: "#contact_information",
+                                                        'data-toggle': "tab",
+                                                        'ng-click': "getContactInformation(); tab_active(2);"
+                                                    });
+                                                } else {
                                                     return false;
                                                 }
                                             }
@@ -660,6 +660,11 @@
                                                     $('.tab-content .tab-pane:nth-child(3)').addClass('active');
                                                     $scope.tab_active(3);
                                                     getDescription();
+                                                    $("li#left-form-each-li-3 a").attr({
+                                                        href: "#description",
+                                                        'data-toggle': "tab",
+                                                        'ng-click': "getDescription(); tab_active(3)"
+                                                    });
                                                 } else {
                                                     return false;
                                                 }
@@ -722,6 +727,11 @@
                                                     $('.tab-content .tab-pane:nth-child(4)').addClass('active');
                                                     $scope.tab_active(4);
                                                     getImage();
+                                                    $("li#left-form-each-li-4 a").attr({
+                                                        href: "#business_image",
+                                                        'data-toggle': "tab",
+                                                        'ng-click': "getImage(); tab_active(4)"
+                                                    });
                                                 } else {
                                                     return false;
                                                 }
@@ -773,8 +783,8 @@
         <?php
         if (IS_BUSINESS_JS_MINIFY == '0') {
             ?>
-                                                                                                                                                                                                                                                                                                                                                                            <!--            <script type="text/javascript" src="<?php echo base_url('assets/js/webpage/business-profile/information.js?ver=' . time()); ?>"></script>
-                                                                                                                                                                                                                                                                                                                                                                            <script type="text/javascript" defer="defer" src="<?php echo base_url('assets/js/webpage/business-profile/common.js?ver=' . time()); ?>"></script>-->
+                                                                                                                                                                                                                                                                                                                                                                                            <!--            <script type="text/javascript" src="<?php echo base_url('assets/js/webpage/business-profile/information.js?ver=' . time()); ?>"></script>
+                                                                                                                                                                                                                                                                                                                                                                                            <script type="text/javascript" defer="defer" src="<?php echo base_url('assets/js/webpage/business-profile/common.js?ver=' . time()); ?>"></script>-->
         <?php } else {
             ?>
             <script type="text/javascript" src="<?php echo base_url('assets/js_min/webpage/business-profile/information.min.js?ver=' . time()); ?>"></script>
