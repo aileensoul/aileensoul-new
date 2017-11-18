64603d21d068be8848cd5e105134d8aae5b2234a
DESKTOP-FNQCPB7\A <ankit.aileensoul@gmail.com>
2017-11-18 14:45:03 +0530

    Business Update
    Business Registration

--- application/front/views/business_profile/business_header2_border.php
+++ application/front/views/business_profile/business_header2_border.php
@@ -202,7 +202,7 @@
                                                 <span class="my_account">
                                                     <div class="my_S">Account</div>
                                                 </span>
-                                                <a href="<?php echo base_url('business-profile/details/' . $businessdata[0]['business_slug']); ?>">
+                                                <a href="<?php echo base_url('business-profile/details/' . $business_common_data[0]['business_slug']); ?>">
                                                     <span class="icon-view-profile edit_data"></span>
                                                     <span> View Profile </span></a> 
                                                 <!--<a href="<?php echo base_url('business-profile/business-information-update'); ?>">-->
@@ -301,7 +301,7 @@
                                                 <span class="my_account">
                                                     <div class="my_S">Account</div>
                                                 </span>
-                                                <a href="<?php echo base_url('business-profile/details/' . $businessdata[0]['business_slug']); ?>">
+                                                <a href="<?php echo base_url('business-profile/details/' . $business_common_data[0]['business_slug']); ?>">
                                                     <span class="icon-view-profile edit_data"></span>
                                                     <span> View Profile </span></a> 
                                                 <!--<a href="<?php //echo base_url('business-profile/business-information-edit');  ?>">-->
