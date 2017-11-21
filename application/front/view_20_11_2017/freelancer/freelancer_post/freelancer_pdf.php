51868e9fc8785fffb09203411e9a08fcda99fb37
DESKTOP-FNQCPB7\A <ankit.aileensoul@gmail.com>
2017-11-21 13:01:59 +0530

    Remove Extra File
    Remove Extra file with business

--- application/front/view_20_11_2017/freelancer/freelancer_post/freelancer_pdf.php
+++
@@ -1,6 +0,0 @@
-
-<style>
-    body{margin: 0;}
-    </style>
-    <!--<input action="action" type="button" value="Back" onclick="history.back();" /> <br/><br/>-->
-<embed src="<?php echo base_url().$this->config->item('free_portfolio_main_upload_path').$freelancerdata[0]['freelancer_post_portfolio_attachment']?>" width="100%" height="100%" style="scroll:">
