f9f72b215650e6dab333bc1c3ff9e9f36e90b88a
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-11-22 16:07:11 +0530

    bus bugs

--- application/front/views/artist/art_profile_live.php
+++ application/front/views/artist/art_profile_live.php
@@ -661,7 +661,7 @@ echo PROFILENA;
                                     </div>
                                     <div class="form-group dob">
                                         <label class="d_o_b"> Date Of Birth :</label>
-                                        <select tabindex="9" class="day" name="selday" id="selday">
+                                       <span> <select tabindex="9" class="day" name="selday" id="selday">
                                             <option value="" disabled selected value>Day</option>
                                             <?php
                                             for ($i = 1; $i <= 31; $i++) {
@@ -670,7 +670,8 @@ echo PROFILENA;
     <?php
 }
 ?>
-                                        </select>
+                                        </select></span>
+                                        <span>
                                         <select tabindex="10" class="month" name="selmonth" id="selmonth">
                                             <option value="" disabled selected value>Month</option>
                                             //<?php
@@ -693,7 +694,8 @@ echo PROFILENA;
 //                  }
 //                  
 ?>
-                                        </select>
+                                        </select></span>
+                                        <span>
                                         <select tabindex="11" class="year" name="selyear" id="selyear">
                                             <option value="" disabled selected value>Year</option>
                                             <?php
@@ -705,16 +707,17 @@ echo PROFILENA;
 ?>
 
                                         </select>
-
+                                        </span>
                                     </div>
                                     <div class="dateerror" style="color:#f00; display: block;"></div>
 
                                     <div class="form-group gender-custom">
+                                        <span>
                                         <select tabindex="12" class="gender"  onchange="changeMe(this)" name="selgen" id="selgen">
                                             <option value="" disabled selected value>Gender</option>
                                             <option value="M">Male</option>
                                             <option value="F">Female</option>
-                                        </select>
+                                        </select></span>
                                     </div>
 
                                     <p class="form-text">
