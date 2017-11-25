b8b189d68773a27d7d80316a4c4f1495a2cc5faa
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-11-25 11:06:52 +0530

    Set the about page

--- application/front/views/about_us/index.php
+++ application/front/views/about_us/index.php
@@ -33,33 +33,251 @@
         ?>
         <link rel="stylesheet" href="<?php echo base_url('assets/css/common-style.css?ver='.time()) ?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/css/style-main.css?ver='.time()) ?>">
-        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        
 
     </head>
     <body class="about-us">
         <div class="main-inner">
-            <?php echo $login_header ?>
+           <div class="terms-con-cus">
+            <header class="terms-con bg-none">
+                <div class="overlaay">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4 col-sm-3">
+                                <h2 class="logo"><a href="<?php echo base_url(); ?>">Aileensoul</a></h2>
+                            </div>
+                            <div class="col-md-8 col-sm-9">
+                                <div class="btn-right pull-right">
+                                <?php if(!$this->session->userdata('aileenuser')) {?>
+                                    <a href="<?php echo base_url('login'); ?>" class="btn2">Login</a>
+                                    <a href="<?php echo base_url('registration'); ?>" class="btn3">Create an account</a>
+                                    <?php }?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <section class="">
+                <div class="main-comtai">
+                    <!-- <h1>Terms and Conditions</h1> -->
+                </div>
+            </section>
+        </div>
             <section class="middle-main">
                 <div class="container">
                     <div class="pt10">
                         <div class="titlea">
-                            <h1 class="pb20">About us</h1>
+                            <h1 class="pb20">what Is Aileensoul</h1>
                         </div>
                         <div class="about-content">
                             Aileensoul is dedicated purely towards providing relentless and free platform to everyone. We provide a diversified platform for every kind of person. You can hire, recruit, and find a job of your preference in your required field. You can also find freelancing work from our site. Aileensoul targets every kind of population be it a person from artistic field or a person working in a contemporary setup. Beginning from hiring a housemaid to hiring an employ for your business, Aileensoul has it all. Any person looking for any kind of job or wants to showcase his/her artistic talent are free to create their profile. We want the gap that exists between the employer and employee to be fulfilled and hence creating a vast platform for employment as well as different services. 
                         </div>
-                        <p class="text-center"><img src="<?php echo base_url('assets/img/message.png'); ?>"></p>
-                        <div class="text-center">
+                       <!--  <p class="text-center"><img src="<?php //echo base_url('assets/img/message.png'); ?>"></p> -->
+                        <!-- <div class="text-center">
                             <ul class="mail-sent">
                                 <li><a title="Email us" href="mailto:hr@aileensoul.com">hr@aileensoul.com</a></li>
                                 <li><a title="Email us" href="mailto:info@aileensoul.com">info@aileensoul.com</a></li>
                                 <li><a title="Email us" href="mailto:inquiry@aileensoul.com">inquiry@aileensoul.com</a></li>
                             </ul>
+                        </div> -->
+                    </div>
+                </div>
+
+                    <div class="container">
+                    <div class="pt10">
+                        <div class="titlea">
+                            <h1 class="pb20">Mission</h1>
+                        </div>
+                        <div class="about-content">
+                            Career is the most important aspect in every person's life and we want to add value to the society by providing a platform to each and every person in the world & help them grow their career.
+
+                      <br>
+                       
+                            AileenSoul works with a mission to free this world from unemployment and poverty, hereby touching everyone's lives.
+
+                        </div>
+                       
+                    </div>
+                </div>
+
+
+
+
+
+                <div class="container">
+                    <div class="pt10">
+                        <div class="titlea">
+                            <h1 class="pb20">Our Team</h1>
+                        </div>
+                       <div class="about-content">
+                           Coming together is a beginning, staying together is progress, and working together is success.<br>
+                            <h6 class="fr" style="margin-right: 150px;">-Henry Ford </h6>
+                        </div>
+                        <div class="all-tem">
+                           <ul class="">
+                            <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/PrashantDadhania.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Nisha Raj</h4>
+                                    <p>Content Head</p>
+                                </div>
+                             </li>
+
+                             <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/yatinbelani.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Yatin Belani</h4>
+                                    <p>Project Manager</p>
+                                </div>
+                             </li>
+
+                             <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/Shashvat.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Shashwat Barbhaya</h4>
+                                    <p>Business Manager</p>
+                                </div>
+                             </li>
+
+                             <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/himanshuSadadiya.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Himanshu Sadadiya</h4>
+                                    <p>AWS Architect/Devops Expert</p>
+                                </div>
+                             </li>
+
+                             <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/AnkitMakadiya.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Ankit Makadiya</h4>
+                                    <p>Technical Head</p>
+                                </div>
+                             </li>
+
+                             <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/KhyatiRaval.jpeg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Khyati Raval </h4>
+                                    <p>Sr. Web Developer</p>
+                                </div>
+                             </li>
+
+                             <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/nikunj.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Nikunj Bhalodiya </h4>
+                                    <p>Software Tester</p>
+                                </div>
+                             </li>
+
+                                <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/Harshad.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Harshad Patoliya</h4>
+                                    <p>Sr. Web Designer</p>
+                                </div>
+                             </li>
+
+                             <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/PRASHANT.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Prashant Dadhaniya</h4>
+                                    <p>Sr. SEO Executive</p>
+                                </div>
+                             </li>
+
+                                <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/jay.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Jay Patel</h4>
+                                    <p>Jr. SEO Executive</p>
+                                </div>
+                             </li>
+
+                                <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/Prashant Dadhania.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Pallavi Panaliya</h4>
+                                    <p>Jr. Web Developer</p>
+                                </div>
+                             </li>
+
+                                
+
+                             </ul>
+
+
+                              <ul class="">
+                            <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/dhaval.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Dhaval Shah</h4>
+                                    <p>Jr. Web Designer</p>
+                                </div>
+                             </li>
+
+
+                             <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/FalguniTank.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Falguni Tank</h4>
+                                    <p>Jr. Web Developer</p>
+                                </div>
+                             </li>
+
+                         </ul>
+
+                          <ul class="Main-im">
+                            <li class="img-custom">
+                                <div class="team-1">
+                                    <img src="assets/img/DhavalShah.jpg">
+                                </div>
+                                 <div class="text-custom">
+                                    <h4>Dhaval Shah</h4>
+                                    <p>CEO</p>
+                                </div>
+                             </li>
+                         </ul>
+
                         </div>
                     </div>
                 </div>
             </section>
+
+
+
+
+
+
             <?php
             echo $login_footer
             ?>
