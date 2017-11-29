27a74192369157136ffd69f3f3324f8e958a0acb
harshad-aileensoul <harshad.aileensoul@gmail.com>
2017-11-29 16:53:43 -0500

    stylechanges

--- application/front/views/login_footer.php
+++ application/front/views/login_footer.php
@@ -12,17 +12,17 @@ if ($this->uri->segment(1) == '' || $this->uri->segment(1) == 'main') {
         <div class="container pt20">
             <div class="row">
 
-                <div class="col-md-8 col-sm-8 pull-right col-xs-12">
-                    <ul>
-                        <li><a title="About Us" href="<?php echo base_url('about-us'); ?>"  target="_blank">About Us</a>|</li>
-                        <li><a href="<?php echo base_url('terms-and-condition'); ?>" title="Terms and Condition" target="_blank">Terms and Condition</a>|</li>
-                        <li><a tabindex="15" href="<?php echo base_url('privacy-policy'); ?>" title="Privacy policy" target="_blank">Privacy policy</a>|</li>
-                        <li><a title="Contact Us" href="<?php echo base_url('contact-us'); ?>"  target="_blank">Contact Us</a>|</li>
-                        <li><a title="Blog" href="<?php echo base_url('blog'); ?>" target="_blank">Blog</a>|</li>
+                <div class="col-md-9 col-sm-12 pull-right col-xs-12">
+                    <ul class="footer-ul">
+                        <li><a title="About Us" href="<?php echo base_url('about-us'); ?>"  target="_blank">About Us</a><span class="mob-none">|</span></li>
+                        <li><a href="<?php echo base_url('terms-and-condition'); ?>" title="Terms and Condition" target="_blank">Terms and Condition</a><span class="mob-none">|</span></li>
+                        <li><a tabindex="15" href="<?php echo base_url('privacy-policy'); ?>" title="Privacy policy" target="_blank">Privacy policy</a><span class="mob-none">|</span></li>
+                        <li><a title="Contact Us" href="<?php echo base_url('contact-us'); ?>"  target="_blank">Contact Us</a><span class="mob-none">|</span></li>
+                        <li><a title="Blog" href="<?php echo base_url('blog'); ?>" target="_blank">Blog</a><span class="mob-none">|</span></li>
                         <li><a title="Send Us Feedback" href="<?php echo base_url('feedback'); ?>" target="_blank">Send Us Feedback</a></li>
                     </ul>
                 </div>
-                <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="col-md-3 col-sm-12 col-xs-12 ftr-copuright">
                     <span>    &#9400; 2017 | by Aileensoul </span>
                 </div>
             </div>
