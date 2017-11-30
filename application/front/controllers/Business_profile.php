30d66f819f7c634a3ef02d421ccccba3afcde3eb
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-11-30 14:40:58 +0530

    Set the css

--- application/front/controllers/Business_profile.php
+++ application/front/controllers/Business_profile.php
@@ -2302,7 +2302,7 @@ Your browser does not support the audio tag.
             if ($status == 0 || $status == " ") {
                 $return_html .= '<div id= "followdiv " class="user_btn">
                                                                                 <button id="follow' . $user['business_profile_id'] . '" onClick="followuser(' . $user['business_profile_id'] . ')">
-                                                                                    Follow 
+                                                                                   <span> Follow </span>
                                                                                 </button></div>';
             } elseif ($status == 1) {
                 $return_html .= '<div id= "unfollowdiv"  class="user_btn" > 
