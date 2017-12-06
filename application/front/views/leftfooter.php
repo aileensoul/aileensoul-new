f08e6b3702281a66688b676d835158d617edacd4
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-12-06 10:23:43 +0530

    Set the page

--- application/front/views/leftfooter.php
+++ application/front/views/leftfooter.php
@@ -1,15 +1,19 @@
 <div class="custom_footer_left fw">
     <div class="fl">
-        <ul>
+        <ul class="cus-full1">
             <li><a href="<?php echo base_url('about-us'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> About Us </a></li>
 
             <li><a href="<?php echo base_url('contact-us'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Contact Us</a></li>
 
-            <li><a href="<?php echo base_url('blog'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Blogs</a></li>
+            <li><a href="<?php echo base_url('blog'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Blog</a></li>
             <li><a href="<?php echo base_url('privacy-policy'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Privacy Policy</a></li>
+        </ul>
+        <ul class="cus-full2">
             <li><a href="<?php echo base_url('terms-and-condition'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Terms &amp; Condition </a></li>
             <li><a title="Disclaimer" href="<?php echo base_url('Disclaimer'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Disclaimer policy</a></li>
 
+        </ul>
+        <ul class="cus-full3">
 
 
             <li><a href="<?php echo base_url('feedback'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Send Us Feedback</a></li>
