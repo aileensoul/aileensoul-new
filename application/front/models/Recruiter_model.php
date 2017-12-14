0824de44b2c36a165333995da8485bacf8c38ba7
khyati@123456 <khyati.aileensoul@gmail.com>
2017-12-14 18:35:33 +0530

    rec cchnaes

--- application/front/models/Recruiter_model.php
+++ application/front/models/Recruiter_model.php
@@ -12,5 +12,15 @@ class Recruiter_model extends CI_Model {
         $result_array = $query->row_array();
         return $result_array;
     }
+    
+//     function getBusinessTypeName($business_type=''){
+//        $business_name = $this->db->select('business_name')->get_where('business_type',array('type_id' => $business_type, 'status'=> '1', 'is_delete' => '0'))->row('business_name');
+//        return $business_name;
+//    }
+    
+//    function getIndustriyalName($industriyal=''){
+//        $industriyal_name = $this->db->select('industry_name')->get_where('industry_type',array('industry_id' => $industriyal, 'status'=> '1', 'is_delete' => '0'))->row('industry_name');
+//        return $industriyal_name;
+//    }
 
 }
