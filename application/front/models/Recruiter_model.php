dfb88f2f912e862782334b8ee9d21eaf1914ce03
khyati@123456 <khyati.aileensoul@gmail.com>
2017-12-15 11:37:40 +0530

    recrutier chnages

--- application/front/models/Recruiter_model.php
+++ application/front/models/Recruiter_model.php
@@ -22,5 +22,20 @@ class Recruiter_model extends CI_Model {
 //        $industriyal_name = $this->db->select('industry_name')->get_where('industry_type',array('industry_id' => $industriyal, 'status'=> '1', 'is_delete' => '0'))->row('industry_name');
 //        return $industriyal_name;
 //    }
+    
+    public function getRecruiterUserChat() {
+        $recruiter_profile_id = $this->data['recruiter_login_profile_id'];
+
+        $recruiter_id = $_POST['recruiter_id'];
+        $user_data = $this->business_model->getBusinessDataBySlug($business_slug, $select_data = "business_profile_id,company_name,business_user_image,other_business_type,other_industrial,business_type,industriyal,business_slug");
+        if ($user_data['business_type'] != '' || $user_data['business_type'] != 'null') {
+            $user_data['business_type'] = $this->business_model->getBusinessTypeName($user_data['business_type']);
+        }
+        if ($user_data['industriyal'] != '' || $user_data['industriyal'] != 'null') {
+            $user_data['industriyal'] = $this->business_model->getIndustriyalName($user_data['industriyal']);
+        }
+        $user_data['chat'] = $this->message_model->getBusinessChat($business_profile_id, $user_data['business_profile_id']);
+        echo json_encode($user_data);
+    }
 
 }
