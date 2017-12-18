2ba16fa9e62b1429df8951eadbe7c2403ae30dd5
ankit-aileensoul <ankit.aileensoul@gmail.com>
2017-12-18 12:52:07 +0530

    login footer
    login footer

--- application/front/views/login_footer.php
+++ application/front/views/login_footer.php
@@ -14,14 +14,14 @@ if ($this->uri->segment(1) == '' || $this->uri->segment(1) == 'main') {
 
                 <div class="fw text-center">
                     <ul class="footer-ul">
-
-
-						<li><a title="Sitemap" href="<?php echo base_url('how-to-use-job-profile-in-aileensoul'); ?>" target="_blank">Job Profile</a></li>
-						<li><a title="Sitemap" href="<?php echo base_url('how-to-use-recruiter-profile-in-aileensoul'); ?>" target="_blank">Recruiter Profile</a></li>
-						<li><a title="Sitemap" href="<?php echo base_url('how-to-use-freelance-profile-in-aileensoul'); ?>" target="_blank">Freelance Profile</a></li>
-						<li><a title="Sitemap" href="<?php echo base_url('how-to-use-business-profile-in-aileensoul'); ?>" target="_blank">Business Profile</a></li>
-						<li><a title="Sitemap" href="<?php echo base_url('how-to-use-artistic-profile-in-aileensoul'); ?>" target="_blank">Artistic Profile</a></li>
-						<li><a title="About Us" href="<?php echo base_url('about-us'); ?>"  target="_blank">About Us</a></li>
+                        <li><a title="Login" href="<?php echo base_url('login'); ?>" target="_blank">Login</a></li>
+                        <li><a title="Create an Account" href="<?php echo base_url('registration'); ?>" target="_blank">Create an Account</a></li>
+                        <li><a title="Job Profile" href="<?php echo base_url('how-to-use-job-profile-in-aileensoul'); ?>" target="_blank">Job Profile</a></li>
+                        <li><a title="Recruiter Profile" href="<?php echo base_url('how-to-use-recruiter-profile-in-aileensoul'); ?>" target="_blank">Recruiter Profile</a></li>
+                        <li><a title="Freelance Profile" href="<?php echo base_url('how-to-use-freelance-profile-in-aileensoul'); ?>" target="_blank">Freelance Profile</a></li>
+                        <li><a title="Business Profile" href="<?php echo base_url('how-to-use-business-profile-in-aileensoul'); ?>" target="_blank">Business Profile</a></li>
+                        <li><a title="Artistic Profile" href="<?php echo base_url('how-to-use-artistic-profile-in-aileensoul'); ?>" target="_blank">Artistic Profile</a></li>
+                        <li><a title="About Us" href="<?php echo base_url('about-us'); ?>"  target="_blank">About Us</a></li>
                         <li><a href="<?php echo base_url('terms-and-condition'); ?>" title="Terms and Condition" target="_blank">Terms and Condition</a></li>
                         <li><a href="<?php echo base_url('privacy-policy'); ?>" title="Privacy policy" target="_blank">Privacy policy</a></li>
                         <li><a title="Disclaimer" href="<?php echo base_url('Disclaimer'); ?>"  target="_blank">Disclaimer policy</a></li>
@@ -29,8 +29,8 @@ if ($this->uri->segment(1) == '' || $this->uri->segment(1) == 'main') {
                         <li><a title="Blog" href="<?php echo base_url('blog'); ?>" target="_blank">Blog</a></li>
                         <li><a title="Send Us Feedback" href="<?php echo base_url('feedback'); ?>" target="_blank">Send Us Feedback</a></li>
                         <li><a title="Sitemap" href="<?php echo base_url('sitemap'); ?>" target="_blank">Sitemap</a></li>
-						
-						 
+
+
                     </ul>
                 </div>
                 <div class="ftr-copuright text-center pt10 pb20 fw">
@@ -40,27 +40,28 @@ if ($this->uri->segment(1) == '' || $this->uri->segment(1) == 'main') {
         </div>
     </footer>
 
-   <!-- IMAGE PRELOADER SCRIPT -->
-<script type="text/javascript">
-//  function preload(arrayOfImages) {
-//     $(arrayOfImages).each(function () {
-//         $('<img />').attr('src',this).appendTo('body').css('display','none');
-//     });
-// }
+    <!-- IMAGE PRELOADER SCRIPT -->
+    <script type="text/javascript">
+        //  function preload(arrayOfImages) {
+        //     $(arrayOfImages).each(function () {
+        //         $('<img />').attr('src',this).appendTo('body').css('display','none');
+        //     });
+        // }
 
-$.fn.preload = function (fn) {
-    var len = this.length, i = 0;
-    return this.each(function () {
-        var tmp = new Image, self = this;
-        if (fn) tmp.onload = function () {
-            fn.call(self, 100 * ++i / len, i === len);
+        $.fn.preload = function (fn) {
+            var len = this.length, i = 0;
+            return this.each(function () {
+                var tmp = new Image, self = this;
+                if (fn)
+                    tmp.onload = function () {
+                        fn.call(self, 100 * ++i / len, i === len);
+                    };
+                tmp.src = this.src;
+            });
         };
-        tmp.src = this.src;
-    });
-};
 
-$('img').preload(function(perc, done) {
-    console.log(this, perc, done);
-});
-</script>
-<!-- IMAGE PRELOADER SCRIPT -->
+        $('img').preload(function (perc, done) {
+            console.log(this, perc, done);
+        });
+    </script>
+    <!-- IMAGE PRELOADER SCRIPT -->
