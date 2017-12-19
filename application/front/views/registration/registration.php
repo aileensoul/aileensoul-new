9a87cbb1c0e73b0f45edb0858d1de8e4a7cb51cc
harshad-aileensoul <harshad.aileensoul@gmail.com>
2017-12-18 20:05:55 -0500

    about us page

--- application/front/views/registration/registration.php
+++ application/front/views/registration/registration.php
@@ -24,7 +24,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-4 col-sm-3 col-xs-3">
-                            <h2 class="logo"><a href="<?php echo base_url(); ?>">Aileensoul</a></h2>
+                            <a href="<?php echo base_url(); ?>"><img src="<?php echo base_url('assets/img/logo-name.png') ?>"></a>
                         </div>
                         <div class="col-md-8 col-sm-9 col-xs-9">
                             <div class="btn-right pull-right t-r-l">
