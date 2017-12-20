ae77848c7ef117355ed19cd5ba179b4e9180f68e
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-12-20 19:44:04 +0530

    Set the page

--- application/front/views/leftfooter.php
+++ application/front/views/leftfooter.php
@@ -18,8 +18,8 @@
 
             <li><a href="<?php echo base_url('feedback'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Send Us Feedback</a></li>
             
-			<li><a class="click-profiles" style="" href="javascript:void(0)"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Profiles <i class="fa fa-sort-asc" aria-hidden="true"></i></a>
-				<div class="click-nav"><ul class="left-ftr-profiles">
+			<li><a id="trigger" class="click-profiles" style="" href="javascript:void(0)"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Profiles <i class="fa fa-sort-asc" aria-hidden="true"></i></a>
+				<div class="click-nav"><ul id="drop" class="left-ftr-profiles">
 					<li><a href="<?php echo base_url('how-to-use-job-profile-in-aileensoul'); ?>" target="_blank">Job Profiles</a></li>
 					<li><a href="<?php echo base_url('how-to-use-recruiter-profile-in-aileensoul'); ?>" target="_blank">Recruiter Profile</a></li>
 					<li><a href="<?php echo base_url('how-to-use-freelance-profile-in-aileensoul'); ?>" target="_blank">Freelance Profile</a></li>
@@ -32,20 +32,15 @@
     </div>
 
 </div>
+
+
 <script>
-	$(".click-profiles").click(function(){
-		$(".left-ftr-profiles").toggle();
-		
-	});
+$('#trigger').click( function(event){
+        event.stopPropagation();
+        $('#drop').toggle();
+    });
+
+    $(document).click( function(){
+        $('#drop').hide();
+    });
 </script>
-<!-- 
-<script>
-$("body").click(function(e){
-        if ($(e.target).is('.click-profiles')) {
-           $('.click-nav').toggle();
-        } else if($('.click-nav').find(e.target).length  == 0) {
-           $('.click-nav').hide();
-        }
-});
-</script>
- -->
