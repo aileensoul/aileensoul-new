73bff4fc2a826a5467c501a2c8c9122966691c8c
ankit-aileensoul <ankit.aileensoul@gmail.com>
2017-12-22 17:55:18 +0530

    alt tag missing
    alt tag missing

--- application/front/views/business_profile/business_following.php
+++ application/front/views/business_profile/business_following.php
@@ -30,7 +30,7 @@
                                     <div class="contact-frnd-post">
                                         <!-- AJAX DATA... -->
                                     </div>
-                                    <div class="fw" id="loader" style="text-align:center;"><img src="<?php echo base_url('assets/images/loader.gif?ver=' . time()) ?>" /></div>
+                                    <div class="fw" id="loader" style="text-align:center;"><img src="<?php echo base_url('assets/images/loader.gif?ver=' . time()) ?>" alt="Loader"/></div>
                                 </div>
                             </div>
                         </div>
