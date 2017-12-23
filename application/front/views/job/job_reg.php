9e48c37545d8724eecbc5cd42678f3553962092b
zalak-aileensoul <zalak.aileensoul@gmail.com>
2017-12-23 13:05:11 +0530

    Set the page

--- application/front/views/job/job_reg.php
+++ application/front/views/job/job_reg.php
@@ -571,7 +571,7 @@
                     <div class="modal-body">
                         <div class="clearfix">
                             <div class=" ">
-                              <div class="title"><h1 style="font-size: 24px;">Sign up First and Register in Job Profile</h1></div>
+                              <div class="title"><h1 style="font-size: 24px;text-transform: none;">Sign up First and Register in Job Profile</h1></div>
                                 <form role="form" name="register_form" id="register_form" method="post">
                                     <div class="row">
                                         <div class="col-sm-6 col-md-6">
