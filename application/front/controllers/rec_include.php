d96815b27540c4db0d5f6da8ecdc13b2591cc9c5
pallavi-aileensoul <pallavi.alieensoul@gmail.com>
2017-12-25 19:10:18 +0530

    updated work

--- application/front/controllers/rec_include.php
+++ application/front/controllers/rec_include.php
@@ -39,4 +39,5 @@ $recdata = $this->data['recdata'] = $this->common->select_data_by_condition('rec
 $contition_array = array('user_id' => $id, 'is_delete' => '0', 're_status' => '1');
 $data = "rec_id,rec_firstname,rec_lastname,rec_email,re_status,rec_phone,re_comp_name,re_comp_email,re_comp_site,re_comp_country,re_comp_state,re_comp_city,user_id,re_comp_profile,re_comp_sector,re_comp_activities,re_step,re_comp_phone,recruiter_user_image,profile_background,profile_background_main,designation,comp_logo";
 $recdata = $this->data['recdata'] = $this->common->select_data_by_condition('recruiter', $contition_array, $data, $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str = array(), $groupby = '');
+
 }
