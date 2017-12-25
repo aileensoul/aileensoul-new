0ef67416709d343badd1d7ef3bbdd39d812ac8d0
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-12-25 12:01:58 +0530

    minify css n js

--- application/front/views/blog/notavalible.php
+++ application/front/views/blog/notavalible.php
@@ -1,12 +1,15 @@
 
 <?php echo $head; ?>
-
+ <?php if (IS_OUTSIDE_CSS_MINIFY == '0'){?>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/gyc.css'); ?>">
+<link rel="stylesheet" href="<?php echo base_url() ?>css/bootstrap.min.css" />
+<?php }else{?>
+
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css_min/gyc.css'); ?>">
+<link rel="stylesheet" href="<?php echo base_url() ?>css_min/bootstrap.min.css" />
+<?php }?>
 
 <?php echo $header; ?>
-
-
-<link rel="stylesheet" href="<?php echo base_url() ?>css/bootstrap.min.css" />
 <body   class="page-container-bg-solid page-boxed">
     <img src="<?php echo base_url() ?>images/404.jpg" alt="404" />
 </body>
