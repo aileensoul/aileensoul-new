30c51c64af88b3d0892b7cda779bc92c70bc136c
pallavi-aileensoul <pallavi.alieensoul@gmail.com>
2017-12-27 12:25:59 +0530

    upadted work

--- application/front/views/freelancer/freelancer_hire/freelancer_add_post_live.php
+++ application/front/views/freelancer/freelancer_hire/freelancer_add_post_live.php
@@ -644,10 +644,10 @@
                                         </div>
 
                                         <div class="form-group">
-                                            <input tabindex="7" type="text" name="email_reg" id="email_reg" class="form-control input-sm" placeholder="Email Address" autocomplete="off">
+                                            <input tabindex="7" type="text" name="email_reg" id="email_reg" class="form-control input-sm" placeholder="Email Address" autocomplete="new-email">
                                         </div>
                                         <div class="form-group">
-                                            <input tabindex="8" type="password" name="password_reg" id="password_reg" class="form-control input-sm" placeholder="Password">
+                                            <input tabindex="8" type="password" name="password_reg" id="password_reg" class="form-control input-sm" placeholder="Password" autocomplete="new-password">
                                             <input type="hidden" name="password_login_postid" id="password_login_postid" class="form-control input-sm post_id_login">
                                         </div>
                                         <div class="form-group dob">
