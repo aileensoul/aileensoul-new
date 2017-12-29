9823b523114371993dcd04acc3509d6244d57fbb
falguni-aileensoul <falguni.aileensoul@gmail.com>
2017-12-29 17:41:26 +0530

    art model

--- application/front/views/artist/user_search.php
+++ application/front/views/artist/user_search.php
@@ -252,7 +252,7 @@
                                         <div class="row">
                                             <div class="col-sm-6 col-md-6">
                                                 <div class="form-group">
-                                                    <input tabindex="101" type="text" name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name">
+                                                    <input tabindex="101" autofocus="" type="text" name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name">
                                                 </div>
                                             </div>
                                             <div class="col-sm-6 col-md-6">
