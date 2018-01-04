f61e4aba6f6a22b05912e2e3266b3e7dae5581e8
falguni-aileensoul <falguni.aileensoul@gmail.com>
2018-01-04 16:56:33 +0530

    art forgot pasword

--- application/front/views/recruiter/rec_liveprofile.php
+++ application/front/views/recruiter/rec_liveprofile.php
@@ -1285,8 +1285,45 @@ $(document).ready(function () {
                             required: "Email Address Is Required.",
                         }
                     },
+                     submitHandler: submitforgotForm
                 });
-                /* validation */
+function submitforgotForm()
+ {
+
+    var email_login = $("#forgot_email").val();
+
+    var post_data = {
+        'forgot_email': email_login,
+//            csrf_token_name: csrf_hash
+    }
+    $.ajax({
+        type: 'POST',
+        url: base_url + 'profile/forgot_live',
+        data: post_data,
+        dataType: "json",
+        beforeSend: function ()
+        {
+            $("#error").fadeOut();
+//            $("#forgotbuton").html('Your credential has been send in your register email id');
+        },
+        success: function (response)
+        {
+            if (response.data == "success") {
+                //  alert("login");
+                $("#forgotbuton").html(response.message);
+                setTimeout(function () {
+                    $('#forgotPassword').modal('hide');
+                    $('#login').modal('show');
+                }, 5000); // milliseconds
+                //window.location = base_url + "job/home/live-post";
+            } else {
+                $("#forgotbuton").html(response.message);
+
+            }
+        }
+    });
+    return false;
+}            /* validation */
 
             });
         </script>
--- assets/js/webpage/recruiter/rec_reg.js
+++ assets/js/webpage/recruiter/rec_reg.js
@@ -562,36 +562,40 @@ function forgot_close() {
         submitHandler: submitforgotForm
     });
     
-          function submitforgotForm()
-    {
+ function submitforgotForm()
+ {
 
-        var email_login = $("#forgot_email").val();
-        
-        var post_data = {
-            'forgot_email': email_login,
+    var email_login = $("#forgot_email").val();
+
+    var post_data = {
+        'forgot_email': email_login,
 //            csrf_token_name: csrf_hash
-        }
-        $.ajax({
-            type: 'POST',
-            url: base_url + 'profile/forgot_live',
-            data: post_data,
-            dataType: "json",
-            beforeSend: function ()
-            {
-                $("#error").fadeOut();
-                $("#forgotbuton").html('Your credential has been send in your register email id');
-            },
-            success: function (response)
-            {
-                if (response.data == "success") {
-                  //  alert("login");
-                    $("#forgotbuton").html(response.data);
-                    //window.location = base_url + "job/home/live-post";
-                } else {
-                    $("#forgotbuton").html(response.message);
-                    
-                }
-            }
-        });
-        return false;
     }
+    $.ajax({
+        type: 'POST',
+        url: base_url + 'profile/forgot_live',
+        data: post_data,
+        dataType: "json",
+        beforeSend: function ()
+        {
+            $("#error").fadeOut();
+//            $("#forgotbuton").html('Your credential has been send in your register email id');
+        },
+        success: function (response)
+        {
+            if (response.data == "success") {
+                //  alert("login");
+                $("#forgotbuton").html(response.message);
+                setTimeout(function () {
+                    $('#forgotPassword').modal('hide');
+                    $('#login').modal('show');
+                }, 5000); // milliseconds
+                //window.location = base_url + "job/home/live-post";
+            } else {
+                $("#forgotbuton").html(response.message);
+
+            }
+        }
+    });
+    return false;
+}            /* validation */
