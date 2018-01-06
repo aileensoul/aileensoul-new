0186ceaf7874bb9111dde6bf0ce379d9d0cafb83
khyati@123456 <khyati.aileensoul@gmail.com>
2018-01-06 18:02:10 +0530

    autocomplete  js

--- application/front/views/autoselecteasy.php
+++ application/front/views/autoselecteasy.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-    
+    <base href="/aileensoul-new/" >
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-route.js"></script>
 
@@ -8,27 +8,30 @@
 
 <p><a href="#/!">Main</a></p>
 
-<a href="red">Red</a>
-<a href="angularurl/green">Green</a>
-<a href="angularurl/blue">Blue</a>
+<a href="user_basic_info/red">Red</a>
+<a href="user_basic_info/green">Green</a>
+<a href="user_basic_info/blue">Blue</a>
 
 <div ng-view></div>
 
 <script>
+var base_url = '<?php echo base_url(); ?>';
+//alert(base_url);
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider,$locationProvider) {
     $routeProvider
-    .when("/", {
+    .when("/user_basic_info/autocomplete", {
         templateUrl : "main.html"
     })
-    .when("/red", {
-        templateUrl : "red.html"
+    .when("/user_basic_info/red", {
+//        templateUrl : "red.html"
+        templateUrl : base_url + "recruiter/red"
     })
-    .when("/green", {
-        templateUrl : "green.html"
+    .when("/user_basic_info/green", {
+        templateUrl : base_url + "recruiter/green"
     })
-    .when("/blue", {
-        templateUrl : "blue.html"
+    .when("/user_basic_info/blue", {
+        templateUrl : base_url + "recruiter/blue"
     });
     
     $locationProvider.html5Mode(true);
