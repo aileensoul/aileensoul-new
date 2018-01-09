0fb49a03d4155c678fe2de9c3459068910637c5c
zalak-aileensoul <zalak.aileensoul@gmail.com>
2018-01-09 14:57:15 +0530

    set the page

--- application/front/views/job/job_liveprintpreview.php
+++ application/front/views/job/job_liveprintpreview.php
@@ -53,10 +53,10 @@ $s3 = new S3(awsAccessKey, awsSecretKey);
      <header>
             <div class="container">
                 <div class="row">
-                    <div class="col-md-4 col-sm-3 col-xs-4 fw-479">
+                    <div class="col-md-4 col-sm-3 left-header text-center fw-479">
                       <a href="<?php echo base_url(); ?>"><img src="<?php echo base_url('assets/img/logo-name.png?ver='.time()) ?>" alt="logo"></a>
                     </div>
-                    <div class="col-md-8 col-sm-9 col-xs-8 fw-479">
+                    <div class="col-md-8 col-sm-9 right-header fw-479 text-center">
                         <div class="btn-right pull-right">
                             <a href="javascript:void(0);" onclick="login_data();" class="btn2">Login</a>
                             <a href="javascript:void(0);" onclick="register_profile();" class="btn3">Create an account</a>
