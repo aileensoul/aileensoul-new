3f561861f8d606b77d95885fa81da0682572f9fe
ankit-aileensoul <ankit.aileensoul@gmail.com>
2018-01-09 17:39:27 +0530

    update
    update

--- application/front/models/User_opportunity.php
+++ application/front/models/User_opportunity.php
@@ -6,11 +6,15 @@ if (!defined('BASEPATH'))
 class User_opportunity extends CI_Model {
 
     public function getContactSuggetion($user_id = '') {
-        $this->db->select("u.user_id,u.first_name,u.last_name,ui.user_image")->from("user u");
+        $this->db->select("u.user_id,u.first_name,u.last_name,ui.user_image,jt.name as title_name,d.degree_name")->from("user u");
         $this->db->join('user_info ui', 'ui.user_id = u.user_id', 'left');
         $this->db->join('user_login ul', 'ul.user_id = u.user_id', 'left');
+        $this->db->join('user_profession up', 'up.user_id = u.user_id', 'left');
+        $this->db->join('job_title jt', 'jt.title_id = up.designation', 'left');
+        $this->db->join('user_student us', 'us.user_id = u.user_id', 'left');
+        $this->db->join('degree d', 'd.degree_id = us.current_study', 'left');
         $this->db->where('u.user_id !=', $user_id);
-        $this->db->order_by('u.user_id', 'DESC');
+        $this->db->order_by('u.user_id', 'ASC');
         $this->db->limit('30');
         $query = $this->db->get();
         $result_array = $query->result_array();
