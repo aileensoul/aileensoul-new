12cbf878dff507efc65c4690436cb281c045aca5
ankit-aileensoul <ankit.aileensoul@gmail.com>
2018-01-10 11:10:01 +0530

    chat model open
    chat model open

--- application/front/models/Chat_model.php
+++ application/front/models/Chat_model.php
@@ -4,7 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Chat_model extends CI_Model {
 
-    function add_message($message, $nickname, $guid, $userid, $id, $message_from_profile, $message_from_profile_id, $message_to_profile, $message_to_profile_id) {
+    //function add_message($message, $nickname, $guid, $userid, $id, $message_from_profile, $message_from_profile_id, $message_to_profile, $message_to_profile_id) {
+    function add_message($message, $guid, $userid, $id, $message_from_profile, $message_from_profile_id, $message_to_profile, $message_to_profile_id) {
         date_default_timezone_set('Asia/Kolkata');
         $data1 = array(
             'message' => (string) $message,
