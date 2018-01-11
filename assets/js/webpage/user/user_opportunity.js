ea07d7c67c504e529833246349b0abb7fde3d404
ankit-aileensoul <ankit.aileensoul@gmail.com>
2018-01-11 12:46:34 +0530

    update
    update

--- assets/js/webpage/user/user_opportunity.js
+++ assets/js/webpage/user/user_opportunity.js
@@ -88,6 +88,123 @@ $('#postfiles').on('click', function () {
     document.getElementById('location').value = c;
     document.getElementById('field').value = d;
 });
+
+// MULTI IMAGE ADD POST START 
+
+var $fileUpload = $("#files"),
+        $list = $('#list'),
+        thumbsArray = [],
+        maxUpload = 5;
+// READ FILE + CREATE IMAGE
+function read(f) {
+    return function (e) {
+        var base64 = e.target.result;
+        var $img = $('<img/>', {
+            src: base64,
+            title: encodeURIComponent(f.name), //( escape() is deprecated! )
+            "class": "thumb"
+        });
+        var $thumbParent = $("<span/>", {
+            html: $img, "class": "thumbParent"}
+        ).append('<span class="remove_thumb"/>');
+        thumbsArray.push(base64);
+        // Push base64 image into array or whatever.
+        $list.append($thumbParent);
+    };
+}
+// HANDLE FILE/S UPLOAD
+function handleFileSelect(e) {
+    e.preventDefault();
+    // Needed?
+    var files = e.target.files;
+    var len = files.length;
+    if (len > maxUpload || thumbsArray.length >= maxUpload) {
+        return alert("Sorry you can upload only 5 images");
+    }
+    for (var i = 0; i < len; i++) {
+        var f = files[i];
+        if (!f.type.match('image.*'))
+            continue;
+        // Only images allowed    
+        var reader = new FileReader();
+        reader.onload = read(f);
+        // Call read() function
+        reader.readAsDataURL(f);
+    }
+}
+$fileUpload.change(function (e) {
+    handleFileSelect(e);
+});
+$list.on('click', '.remove_thumb', function () {
+    var $removeBtns = $('.remove_thumb');
+    // Get all of them in collection
+    var idx = $removeBtns.index(this);
+    // Exact Index-from-collection
+    $(this).closest('span.thumbParent').remove();
+    // Remove tumbnail parent
+    thumbsArray.splice(idx, 1);
+    // Remove from array
+});
+
+
+$('#file-fr').fileinput({
+    language: 'fr',
+    uploadUrl: '#',
+    allowedFileExtensions: ['jpg', 'JPG', 'jpeg', 'JPEG', 'PNG', 'png', 'gif', 'GIF', 'psd', 'PSD', 'bmp', 'BMP', 'tiff', 'TIFF', 'iff', 'IFF', 'xbm', 'XBM', 'webp', 'WebP', 'HEIF', 'heif', 'BAT', 'bat', 'BPG', 'bpg', 'SVG', 'svg', 'mp4', 'mp3', 'pdf']
+});
+$('#file-es').fileinput({
+    language: 'es',
+    uploadUrl: '#',
+    allowedFileExtensions: ['jpg', 'JPG', 'jpeg', 'JPEG', 'PNG', 'png', 'gif', 'GIF', 'psd', 'PSD', 'bmp', 'BMP', 'tiff', 'TIFF', 'iff', 'IFF', 'xbm', 'XBM', 'webp', 'WebP', 'HEIF', 'heif', 'BAT', 'bat', 'BPG', 'bpg', 'SVG', 'svg', 'mp4', 'mp3', 'pdf']
+});
+$("#postfiles").fileinput({
+    uploadUrl: '#', // you must set a valid URL here else you will get an error
+    allowedFileExtensions: ['jpg', 'JPG', 'jpeg', 'JPEG', 'PNG', 'png', 'gif', 'GIF', 'psd', 'PSD', 'bmp', 'BMP', 'tiff', 'TIFF', 'iff', 'IFF', 'xbm', 'XBM', 'webp', 'WebP', 'HEIF', 'heif', 'BAT', 'bat', 'BPG', 'bpg', 'SVG', 'svg', 'mp4', 'mp3', 'pdf'],
+    overwriteInitial: false,
+    maxFileSize: 1000000,
+    maxFilesNum: 10,
+    //allowedFileTypes: ['image','video', 'flash'],
+    slugCallback: function (filename) {
+        return filename.replace('(', '_').replace(']', '_');
+    }
+});
+
+$(document).ready(function () {
+    $("#test-upload").fileinput({
+        'showPreview': false,
+        'allowedFileExtensions': ['jpg', 'JPG', 'jpeg', 'JPEG', 'PNG', 'png', 'gif', 'GIF', 'psd', 'PSD', 'bmp', 'BMP', 'tiff', 'TIFF', 'iff', 'IFF', 'xbm', 'XBM', 'webp', 'WebP', 'HEIF', 'heif', 'BAT', 'bat', 'BPG', 'bpg', 'SVG', 'svg', 'mp4', 'mp3', 'pdf'],
+        'elErrorContainer': '#errorBlock'
+    });
+    $("#kv-explorer").fileinput({
+        'theme': 'explorer',
+        'uploadUrl': '#',
+        overwriteInitial: false,
+        initialPreviewAsData: true,
+        initialPreview: [
+            "http://lorempixel.com/1920/1080/nature/1",
+            "http://lorempixel.com/1920/1080/nature/2",
+            "http://lorempixel.com/1920/1080/nature/3",
+        ],
+        initialPreviewConfig: [
+            {
+                caption: "nature-1.jpg", size: 329892, width: "120px", url: "{$url}", key: 1}
+            ,
+            {
+                caption: "nature-2.jpg", size: 872378, width: "120px", url: "{$url}", key: 2}
+            ,
+            {
+                caption: "nature-3.jpg", size: 632762, width: "120px", url: "{$url}", key: 3}
+            ,
+        ]
+    });
+});
+
+// MULTI IMAGE ADD POST START 
+
+
+
+
+
 $(window).on("load", function () {
     $(".custom-scroll").mCustomScrollbar({
         autoHideScrollbar: true,
