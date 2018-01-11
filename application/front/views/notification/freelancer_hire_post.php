1932af3839e26bef87296dda30bd48310d8efb1f
pallavi-aileensoul <pallavi.alieensoul@gmail.com>
2018-01-11 15:50:52 +0530

    updated work

--- application/front/views/notification/freelancer_hire_post.php
+++ application/front/views/notification/freelancer_hire_post.php
@@ -312,4 +312,160 @@ $freehiredata = $this->freelancer_hire_model->getfreelancerhiredata($freelancerp
 </html>
 <script>
     var base_url = '<?php echo base_url(); ?>';
+    //SCRIPT FOR AUTOFILL OF SEARCH KEYWORD START
+
+$(function () {
+    function split(val) {
+        return val.split(/,\s*/);
+    }
+    function extractLast(term) {
+        return split(term).pop();
+    }
+    $(".skill_keyword").bind("keydown", function (event) {
+        if (event.keyCode === $.ui.keyCode.TAB &&
+                $(this).autocomplete("instance").menu.active) {
+            event.preventDefault();
+        }
+    })
+            .autocomplete({
+                minLength: 2,
+                source: function (request, response) {
+                    // delegate back to autocomplete, but extract the last term
+                    $.getJSON(base_url + "freelancer/freelancer_apply_search_keyword", {term: extractLast(request.term)}, response);
+                },
+                focus: function () {
+                    // prevent value inserted on focus
+                    return false;
+                },
+                select: function (event, ui) {
+
+                    var terms = split(this.value);
+                    if (terms.length <= 1) {
+                        // remove the current input
+                        terms.pop();
+                        // add the selected item
+                        terms.push(ui.item.value);
+                        // add placeholder to get the comma-and-space at the end
+                        terms.push("");
+                        this.value = terms.join("");
+                        return false;
+                    } else {
+                        var last = terms.pop();
+                        $(this).val(this.value.substr(0, this.value.length - last.length - 2)); // removes text from input
+                        $(this).effect("highlight", {}, 1000);
+                        $(this).attr("style", "border: solid 1px red;");
+                        return false;
+                    }
+                }
+            });
+});
+
+//SCRIPT FOR AUTOFILL OF SEARCH KEYWORD END
+
+
+//SCRIPT FOR CITY AUTOFILL OF SEARCH START
+
+$(function () {
+    function split(val) {
+        return val.split(/,\s*/);
+    }
+    function extractLast(term) {
+        return split(term).pop();
+    }
+    $(".skill_place").bind("keydown", function (event) {
+        if (event.keyCode === $.ui.keyCode.TAB &&
+                $(this).autocomplete("instance").menu.active) {
+            event.preventDefault();
+        }
+    })
+            .autocomplete({
+                minLength: 2,
+                source: function (request, response) {
+                    // delegate back to autocomplete, but extract the last term
+                    $.getJSON(base_url + "freelancer/freelancer_search_city", {term: extractLast(request.term)}, response);
+                },
+                focus: function () {
+                    // prevent value inserted on focus
+                    return false;
+                },
+                select: function (event, ui) {
+
+                    var terms = split(this.value);
+                    if (terms.length <= 1) {
+                        // remove the current input
+                        terms.pop();
+                        // add the selected item
+                        terms.push(ui.item.value);
+                        // add placeholder to get the comma-and-space at the end
+                        terms.push("");
+                        this.value = terms.join("");
+                        return false;
+                    } else {
+                        var last = terms.pop();
+                        $(this).val(this.value.substr(0, this.value.length - last.length - 2)); // removes text from input
+                        $(this).effect("highlight", {}, 1000);
+                        $(this).attr("style", "border: solid 1px red;");
+                        return false;
+                    }
+                }
+            });
+});
+
+//SCRIPT FOR CITY AUTOFILL OF SEARCH END
+
+//CHECK SEARCH KEYWORD AND LOCATION BLANK START
+function checkvalue() {
+  
+    var searchkeyword1 = $.trim(document.getElementById('tags').value);
+
+    //  var searchkeyword = searchkeyword.replace(' ', '-');
+    // var searchkeyword = searchkeyword.replace(/[^' ']/g, '-');
+    var searchkeyword = searchkeyword1.replace(/\s/g, '-');
+    var searchkeyword = searchkeyword.replace(/[^a-zA-Z0-9\-]/g, '');
+    var searchkeyword = searchkeyword.replace(/-+/g, "-");
+
+    var searchplace1 = $.trim(document.getElementById('searchplace').value);
+    var searchplace = searchplace1.replace(' ', '-');
+    var searchplace = searchplace.replace(/[^a-zA-Z0-9\-]/g, '');
+
+    
+
+    if (searchkeyword == "" && searchplace == "") {
+     
+        return false;
+    } else {
+       
+        if (searchkeyword == "") {
+            window.location = base_url + 'project-in-' + searchplace;
+            return false;
+        } else if (searchplace == "") {
+            if (searchkeyword == 'projects') {
+                window.location = base_url + 'projects';
+            } else {
+
+                $.ajax({
+                    type: 'POST',
+                    url: base_url + "freelancer/session",
+                    data: 'keyword=' + encodeURIComponent(searchkeyword1) ,
+                    success: function (data) {
+                       window.location = base_url + searchkeyword + '-project';
+                    }
+                });
+              //  window.location = base_url + searchkeyword + '-project';
+            }
+            return false;
+        } else {
+             $.ajax({
+                    type: 'POST',
+                    url: base_url + "freelancer/session",
+                    data: 'keyword=' + encodeURIComponent(searchkeyword1) + '&keyword1=' + encodeURIComponent(searchplace1) ,
+                    success: function (data) {
+                       window.location = base_url + searchkeyword + '-project-in-' + searchplace;
+                    }
+                });
+            
+            return false;
+        }
+    }
+}
 </script>
