66ab711aa0d324763dc6a45dd2bd17c45bffa1bd
zalak-aileensoul <zalak.aileensoul@gmail.com>
2018-01-16 18:51:13 +0530

    Set the css

--- application/front/views/job/job_education.php
+++ application/front/views/job/job_education.php
@@ -955,14 +955,14 @@
    </section>
    <!-- Bid-modal  --> 
       <div class="modal fade message-box biderror custom-message in" id="bidmodal" role="dialog"  >
-        <!--  <div class="modal-dialog modal-lm" >
+         <div class="modal-dialog modal-lm" >
             <div class="modal-content message">
                <button type="button" class="modal-close" data-dismiss="modal">&times;</button>       
                <div class="modal-body">
                   <span class="mes"></span>
                </div>
             </div>
-         </div> -->
+         </div>
       </div>
       <!-- Model Popup Close -->
 
