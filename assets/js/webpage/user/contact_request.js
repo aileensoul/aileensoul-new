d6d9f8e293ceaded77c82b1930b0e80d51a98922
ankit-aileensoul <ankit.aileensoul@gmail.com>
2018-01-26 15:26:58 +0530

    contact request

---
+++ assets/js/webpage/user/contact_request.js
@@ -0,0 +1,46 @@
+app.controller('contactRequestController', function ($scope, $http) {
+    pending_contact_request();
+    function pending_contact_request() {
+        $http({
+            method: 'POST',
+            url: base_url + 'userprofile_page/pending_contact_request',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(function (success) {
+            pending_contact_request = success.data;
+            $scope.pending_contact_request_data = pending_contact_request;
+        });
+    }
+    $scope.confirmContact = function (from_id, index) {
+        $http({
+            method: 'POST',
+            url: base_url + 'userprofile/contactRequestAction',
+            data: 'from_id=' + from_id + '&action=confirm',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(function (success) {
+            if (success.data) {
+                alert(12121);
+                $scope.pending_contact_request_data[index].splice(0, 1);
+            }
+        });
+    }
+
+    $scope.rejectContact = function (from_id, index, contact) {
+        $http({
+            method: 'POST',
+            url: base_url + 'userprofile/contactRequestAction',
+            data: 'from_id=' + from_id + '&action=reject',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(function (success) {
+            if (success.data) {
+                $scope.pending_contact_request_data[index].splice(0, 1);
+            }
+        });
+    }
+
+});
+$(window).on("load", function () {
+    $(".custom-scroll").mCustomScrollbar({
+        autoHideScrollbar: true,
+        theme: "minimal"
+    });
+});
