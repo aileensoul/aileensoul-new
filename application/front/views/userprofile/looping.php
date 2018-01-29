1e1bda84aceaa994b4a3ffecd86019a0694cfe32
khyati@123456 <khyati.aileensoul@gmail.com>
2018-01-29 15:33:27 +0530

    user post model chnages

--- application/front/views/userprofile/looping.php
+++ application/front/views/userprofile/looping.php
@@ -1,7 +1,9 @@
  <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+ <script src="<?php echo base_url('assets/js/infinite-scroll.js'); ?>"></script>
 
 <div ng-app='myApp' ng-controller='DemoController'>
+  <div infinite-scroll='loadMore()' infinite-scroll-distance='2'>
       <!--<div ng-repeat='image in images'>-->
           <div ng-if="postData.length != 0" class="all-post-box" ng-repeat="post in postData">
                 <input type="hidden" name="page_number" class="page_number" ng-class="page_number" ng-model="post.page_number" ng-value="{{post.page_data.page}}">
@@ -182,41 +184,20 @@
             </div>
       <!--</div>-->
     <!--<img ng-repeat='image in images' ng-src='http://placehold.it/225x250&text={{image}}'><br>-->
- 
+  </div>
 </div>
 <script type='text/javascript'>
       var base_url = '<?php echo base_url(); ?>';
        var user_slug = 'dhaval-shah';
 var myApp = angular.module('myApp', ['infinite-scroll']);
 myApp.controller('DemoController', function($scope,$http) {
-    
     getUserDashboardPost();
-    
     function getUserDashboardPost(pagenum = '') { alert(13);
         $('#loader').show();
         $http.get(base_url + "user_post/getUserDashboardPost?page=" + pagenum + "&user_slug=" + user_slug).then(function (success) {alert(success.data);
             $('#loader').hide();
             $scope.postData = success.data;
-          //  check_no_post_data();
-           // $('video,audio').mediaelementplayer(/* Options */);
         }, function (error) {});
     }
-  //$scope.images = [1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
-//  $scope.images = [1, 2, 3];
-
-  $scope.loadMore = function() { //alert(111)
-    var last = $scope.postData[$scope.postData.length - 1];
-   // alert(last);
-    $http.get(base_url + "user_post/getUserDashboardPost?page=" + pagenum + "&user_slug=" + user_slug).then(function (success) {alert(success.data);
-            $('#loader').hide();
-            //$scope.postData = success.data;
-            $scope.postData.push(last + success.data);
-          //  check_no_post_data();
-           // $('video,audio').mediaelementplayer(/* Options */);
-        }, function (error) {});
-//    for(var i = 1; i <= 8; i++) {
-//      $scope.images.push(last + i);
-//    }
-  };
 });
 </script>
