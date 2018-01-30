12bad153dacfcd2830dee1b69cd8d5f5b8585c89
khyati@123456 <khyati.aileensoul@gmail.com>
2018-01-30 13:21:16 +0530

    loadmore dta

--- application/front/models/userprofile_model.php
+++ application/front/models/userprofile_model.php
@@ -169,7 +169,7 @@ class Userprofile_model extends CI_Model {
         $user_post = $query->result_array();
 
         foreach ($user_post as $key => $value) {
-            $result_array[$key]['post_data'] = $user_post[$key];
+             $result_array[$key]['post_data'] = $user_post[$key];
 
             $this->db->select("count(*) as file_count")->from("user_post_file upf");
             $this->db->where('upf.post_id', $value['id']);
@@ -177,9 +177,67 @@ class Userprofile_model extends CI_Model {
             $total_post_files = $query->row_array('file_count');
             $result_array[$key]['post_data']['total_post_files'] = $total_post_files['file_count'];
 
-//            $result_array[$key]['page_data']['page'] = $page;
-//            $result_array[$key]['page_data']['total_record'] = $this->userPostCount($user_id);
-//            $result_array[$key]['page_data']['perpage_record'] = $limit;
+            $this->db->select("u.user_id,u.user_slug,CONCAT(u.first_name,' ',u.last_name) as fullname,ui.user_image,jt.name as title_name,d.degree_name")->from("user u");
+            $this->db->join('user_info ui', 'ui.user_id = u.user_id', 'left');
+            $this->db->join('user_login ul', 'ul.user_id = u.user_id', 'left');
+            $this->db->join('user_profession up', 'up.user_id = u.user_id', 'left');
+            $this->db->join('job_title jt', 'jt.title_id = up.designation', 'left');
+            $this->db->join('user_student us', 'us.user_id = u.user_id', 'left');
+            $this->db->join('degree d', 'd.degree_id = us.current_study', 'left');
+            $this->db->where('u.user_id', $value['user_id']);
+            $query = $this->db->get();
+            $user_data = $query->row_array();
+            $result_array[$key]['user_data'] = $user_data;
+
+            if ($value['post_for'] == 'opportunity') {
+                $this->db->select("uo.post_id,GROUP_CONCAT(DISTINCT(jt.name)) as opportunity_for,GROUP_CONCAT(DISTINCT(c.city_name)) as location,uo.opportunity,it.industry_name as field")->from("user_opportunity uo, ailee_job_title jt, ailee_cities c");
+                $this->db->join('industry_type it', 'it.industry_id = uo.field', 'left');
+                $this->db->where('uo.id', $value['post_id']);
+                $this->db->where('FIND_IN_SET(jt.title_id, uo.`opportunity_for`) !=', 0);
+                $this->db->where('FIND_IN_SET(c.city_id, uo.`location`) !=', 0);
+                $this->db->group_by('uo.opportunity_for', 'uo.location');
+                $query = $this->db->get();
+                $opportunity_data = $query->row_array();
+                $result_array[$key]['opportunity_data'] = $opportunity_data;
+            } elseif ($value['post_for'] == 'simple') {
+                $this->db->select("usp.description")->from("user_simple_post usp");
+                $this->db->where('usp.id', $value['post_id']);
+                $query = $this->db->get();
+                $simple_data = $query->row_array();
+                $result_array[$key]['simple_data'] = $simple_data;
+            } elseif ($value['post_for'] == 'question') {
+                $this->db->select("uaq.*,GROUP_CONCAT(DISTINCT(t.name)) as category,it.industry_name as field")->from("user_ask_question uaq, ailee_tags t");
+                $this->db->join('industry_type it', 'it.industry_id = uaq.field', 'left');
+                $this->db->where('uaq.id', $value['post_id']);
+                $this->db->where('FIND_IN_SET(t.id, uaq.`category`) !=', 0);
+                $this->db->group_by('uaq.category');
+                $query = $this->db->get();
+                $question_data = $query->row_array();
+                $result_array[$key]['question_data'] = $question_data;
+            }
+            $this->db->select("upf.file_type,upf.filename")->from("user_post_file upf");
+            $this->db->where('upf.post_id', $value['id']);
+            $query = $this->db->get();
+            $post_file_data = $query->result_array();
+            $result_array[$key]['post_file_data'] = $post_file_data;
+
+            $post_like_data = $this->postLikeData($value['id']);
+            $post_like_count = $this->likepost_count($value['id']);
+            $result_array[$key]['post_like_count'] = $post_like_count;
+            $result_array[$key]['is_userlikePost'] = $this->is_userlikePost($user_id, $value['id']);
+            if ($post_like_count > 1) {
+                $result_array[$key]['post_like_data'] = $post_like_data['username'] . ' and ' . ($post_like_count - 1) . ' other';
+            } elseif ($post_like_count == 1) {
+                $result_array[$key]['post_like_data'] = $post_like_data['username'];
+            }
+            $result_array[$key]['post_comment_count'] = $this->postCommentCount($value['id']);
+            $result_array[$key]['post_comment_data'] = $postCommentData = $this->postCommentData($value['id']);
+
+            foreach ($postCommentData as $key1 => $value1) {
+                $result_array[$key]['post_comment_data'][$key1]['is_userlikePostComment'] = $this->is_userlikePostComment($user_id, $value1['comment_id']);
+                $result_array[$key]['post_comment_data'][$key1]['postCommentLikeCount'] = $this->postCommentLikeCount($value1['comment_id']) == '0' ? '' : $this->postCommentLikeCount($value1['comment_id']);
+            }
+
         }
 
         $page_array['page'] = $page;
