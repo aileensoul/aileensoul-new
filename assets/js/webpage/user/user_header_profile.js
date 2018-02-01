22aa1b684a43758c20be628c304467a141dbdf67
khyati@123456 <khyati.aileensoul@gmail.com>
2018-02-01 19:49:23 +0530

    user header profile

--- assets/js/webpage/user/user_header_profile.js
+++ assets/js/webpage/user/user_header_profile.js
@@ -29,7 +29,6 @@ app.controller('headerCtrl', function ($scope, $http) {
     }
 
     $scope.header_all_profile = function () {
-        alert(header_all_profile);
         $('.all .dropdown-menu').html(header_all_profile);
     }
 
