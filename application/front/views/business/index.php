ac5153a7d5680731c5a41fa89c6c1cb0f7360ed1
ankit-aileensoul <ankit.aileensoul@gmail.com>
2018-02-01 18:09:19 +0530

    update
    update

--- application/front/views/business/index.php
+++ application/front/views/business/index.php
@@ -44,43 +44,21 @@
                         </div>
                         <div class="cat-box">
                             <ul>
-                                <li>
-                                    <a href="">
+                                <li ng-repeat="category in businessCategory">
+                                    <a href="{{category.industry_id}}">
                                         <img src="<?php echo base_url('assets/n-images/car.png') ?>">
-                                        <p>Car & vehicles<span>(250)</span><p>
+                                        <p>{{category.industry_name}}<span>({{category.count}})</span><p>
 
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="">
+                                    <a href="{{category.industry_id}}">
                                         <img src="<?php echo base_url('assets/n-images/car.png') ?>">
-                                        <p>Car & vehicles<span>(250)</span><p>
-
+                                        <p>Other<span>({{otherCategoryCount}})</span><p>
                                     </a>
                                 </li>
-                                <li>
-                                    <a href="">
-                                        <img src="<?php echo base_url('assets/n-images/car.png') ?>">
-                                        <p>Car & vehicles<span>(250)</span><p>
-
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="">
-                                        <img src="<?php echo base_url('assets/n-images/car.png') ?>">
-                                        <p>Car & vehicles<span>(250)</span><p>
-
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="">
-                                        <img src="<?php echo base_url('assets/n-images/car.png') ?>">
-                                        <p>Car & vehicles<span>(250)</span><p>
-                                    </a>
-                                </li>
-
                             </ul>
-                            <p class="text-center"><a href="business-category.html" class="btn-1">View More</a></p>
+                            <p class="text-center"><a href="<?php echo base_url('business-profile/category') ?>" class="btn-1">View More</a></p>
                         </div>
                     </div>
                 </div>
@@ -191,189 +169,6 @@
                 </div>
             </div>
         </div>
-        <!--  poup modal  -->
-        <div style="display:none;" class="modal fade" id="post-popup1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <button type="button" class="modal-close" data-dismiss="modal">×</button>
-                    <div class="post-popup-box">
-                        <form>
-                            <div class="post-box">
-                                <div class="post-img">
-                                    <img src="img/user-pic.jpg">
-                                </div>
-                                <div class="post-text">
-                                    <textarea class="title-text-area" placeholder="Post Opportunity"></textarea>
-                                </div>
-                                <div class="all-upload">
-                                    <label for="file-1">
-                                        <i class="fa fa-camera upload_icon"><span class="upload_span_icon"> Photo </span></i>
-                                        <i class="fa fa-video-camera upload_icon"><span class="upload_span_icon"> Video</span>  </i> 
-                                        <i class="fa fa-music upload_icon"> <span class="upload_span_icon">  Audio </span> </i>
-                                        <i class="fa fa-file-pdf-o upload_icon"><span class="upload_span_icon"> PDF </span></i>
-                                    </label>
-                                </div>
-                                <div class="post-box-bottom">
-                                    <ul>
-                                        <li>
-                                            <a href="" data-target="#post-popup" data-toggle="modal">
-                                                <img src="img/post-op.png"><span>Post Opportunity</span>
-                                            </a>
-                                        </li>
-                                        <li class="pl15">
-                                            <a href="article.html">
-                                                <img src="img/article.png"><span>Post Article</span>
-                                            </a>
-                                        </li>
-                                        <li class="pl15">
-                                            <a href="" data-target="#ask-question" data-toggle="modal">
-                                                <img src="img/ask-qustion.png"><span>Ask Quastion</span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                    <p class="pull-right">
-                                        <button type="submit" class="btn1" value="Submit">Post</button>
-                                    </p>
-                                </div>
-
-                            </div>
-
-
-                        </form>
-                    </div>
-
-
-
-                </div>
-            </div>
-
-        </div>
-        <div style="display:none;" class="modal fade" id="post-popup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <button type="button" class="modal-close" data-dismiss="modal">×</button>
-                    <div class="post-popup-box">
-                        <form>
-                            <div class="post-box">
-                                <div class="post-img">
-                                    <img src="img/user-pic.jpg">
-                                </div>
-                                <div class="post-text">
-                                    <textarea class="title-text-area" placeholder="Post Opportunity"></textarea>
-                                </div>
-                                <div class="all-upload">
-                                    <label for="file-1">
-                                        <i class="fa fa-camera upload_icon"><span class="upload_span_icon"> Photo </span></i>
-                                        <i class="fa fa-video-camera upload_icon"><span class="upload_span_icon"> Video</span>  </i> 
-                                        <i class="fa fa-music upload_icon"> <span class="upload_span_icon">  Audio </span> </i>
-                                        <i class="fa fa-file-pdf-o upload_icon"><span class="upload_span_icon"> PDF </span></i>
-                                    </label>
-                                </div>
-
-                            </div>
-                            <div class="post-field">
-
-                                <div id="content" class="form-group">
-                                    <label>FOR WHOM THIS OPPORTUNITY ?<span class="pull-right"><img src="img/tooltip.png"></span></label>
-                                    <textarea rows="1" max-rows="5" placeholder="Ex:Seeking Opportunity, CEO, Enterpreneur, Founder, Singer, Photographer, PHP Developer, HR, BDE, CA, Doctor, Freelancer.." cols="10" style="resize:none"></textarea>
-
-                                </div>
-                                <div class="form-group">
-                                    <label>WHICH LOCATION?<span class="pull-right"><img src="img/tooltip.png"></span></label>
-                                    <textarea type="text" class="" placeholder="Ex:Mumbai, Delhi, New south wels, London, New York, Captown, Sydeny, Shanghai, Moscow, Paris, Tokyo.. "></textarea>
-
-                                </div>
-                                <div class="form-group">
-                                    <label>What is your field?<span class="pull-right"><img src="img/tooltip.png"></span></label>
-                                    <select>
-                                        <option>What is your field</option>
-                                        <option>IT</option>
-                                        <option>Teacher</option>
-                                        <option>Sports</option>
-                                    </select>
-                                </div>
-
-
-
-
-
-                            </div>
-                            <div class="text-right fw pt10 pb20 pr15">
-                                <button type="submit" class="btn1"  value="Submit">Post</button> 
-                            </div>
-                        </form>
-                    </div>
-
-
-
-                </div>
-            </div>
-
-        </div>
-        <div style="display:none;" class="modal fade" id="ask-question" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <button type="button" class="modal-close" data-dismiss="modal">×</button>
-                    <div class="post-popup-box">
-                        <form>
-                            <div class="post-box">
-                                <div class="post-img">
-                                    <img src="img/user-pic.jpg">
-                                </div>
-                                <div class="post-text">
-                                    <textarea class="title-text-area" placeholder="Ask Quastion"></textarea>
-                                </div>
-                                <div class="all-upload">
-                                    <label for="file-1">
-                                        <i class="fa fa-camera upload_icon"><span class="upload_span_icon"> Add Screenshot </span></i>
-                                        <i class="fa fa fa-link upload_icon"><span class="upload_span_icon"> Add Link</span>  </i> 
-
-                                    </label>
-                                </div>
-
-                            </div>
-                            <div class="post-field">
-
-                                <div class="form-group">
-                                    <label>Add Description<span class="pull-right"><img src="img/tooltip.png"></span></label>
-                                    <textarea rows="1" max-rows="5" placeholder="Add Description" cols="10" style="resize:none"></textarea>
-
-                                </div>
-                                <div class="form-group">
-                                    <label>Related Categories<span class="pull-right"><img src="img/tooltip.png"></span></label>
-                                    <input type="text" class="" placeholder="Related Categories">
-
-                                </div>
-                                <div class="form-group">
-                                    <label>From which field the Question asked?<span class="pull-right"><img src="img/tooltip.png"></span></label>
-                                    <select>
-                                        <option>What is your field</option>
-                                        <option>IT</option>
-                                        <option>Teacher</option>
-                                        <option>Sports</option>
-                                    </select>
-                                </div>
-
-
-
-
-
-                            </div>
-                            <div class="text-right fw pt10 pb20 pr15">
-                                <button type="submit" class="btn1"  value="Submit">Post</button> 
-                            </div>
-                        </form>
-                    </div>
-
-
-
-                </div>
-            </div>
-
-        </div>
         <script src="<?php echo base_url('assets/js/jquery.min.js?ver=' . time()) ?>"></script>
         <script src="<?php echo base_url('assets/js/bootstrap.min.js?ver=' . time()) ?>"></script>
         <script src="<?php echo base_url('assets/js/owl.carousel.min.js?ver=' . time()) ?>"></script>
@@ -381,17 +176,13 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
         <script data-semver="0.13.0" src="http://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.13.0.min.js"></script>
-        <script src="<?php echo base_url('assets/js/angular-validate.min.js?ver=' . time()) ?>"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-route.js"></script>
-        <script src="<?php echo base_url('assets/js/ng-tags-input.min.js?ver=' . time()); ?>"></script>
-        <script src="<?php echo base_url('assets/js/angular/angular-tooltips.min.js?ver=' . time()); ?>"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-sanitize.js"></script>
         <script>
             var base_url = '<?php echo base_url(); ?>';
             var user_id = '<?php echo $this->session->userdata('aileenuser'); ?>';
             var title = '<?php echo $title; ?>';
             var header_all_profile = '<?php echo $header_all_profile; ?>';
-            var app = angular.module('businessApp', ['ui.bootstrap', 'ngTagsInput', 'ngSanitize']);
+            var app = angular.module('businessApp', ['ui.bootstrap']);
         </script>               
         <script src="<?php echo base_url('assets/js/webpage/user/user_header_profile.js?ver=' . time()) ?>"></script>
         <script src="<?php echo base_url('assets/js/webpage/business/index.js?ver=' . time()) ?>"></script>
