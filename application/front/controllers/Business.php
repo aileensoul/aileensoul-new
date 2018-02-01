ad2ed53fd3e48d9b961f46a99bcf84ec92b61f42
khyati@123456 <khyati.aileensoul@gmail.com>
2018-02-01 18:24:56 +0530

    sdkds

--- application/front/controllers/Business.php
+++ application/front/controllers/Business.php
@@ -74,7 +74,7 @@ class Business extends MY_Controller {
         $inddata = $this->common->select_data_by_condition('industry_type', $contition_array, $data = 'industry_id,industry_name', $sortby = '', $orderby = '', $limit = '', $offset = '', $join_str5 = '', $groupby = '');
 
         foreach ($inddata as $k => $v) {
-            $data = array('industry_slug' => $this->common->clean($v['industry_name']));
+            $data = array('industry_slug' => strtolower($this->common->clean($v['industry_name'])));
             $insert_id = $this->common->update_data($data, 'industry_type', 'industry_id', $v['industry_id']);
         }
         echo "yes";
