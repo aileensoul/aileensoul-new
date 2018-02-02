8837f4528cfe67cda8c74d4c5cb2017b43900f1d
ankit-aileensoul <ankit.aileensoul@gmail.com>
2018-02-02 13:21:15 +0530

    busines cateory
    busines cateory

---
+++ application/front/views/business/search_banner.php
@@ -0,0 +1,20 @@
+<div class="search-banner">
+    <div class="container">
+        <div class="text-right pt20">
+            <a class="btn5" href="<?php echo base_url('business-profile/business-information') ?>">Create Business Profile</a>
+        </div>
+        <div class="search-bnr-text">
+            <h1>Find The Business That Fits Your Life</h1>
+        </div>
+        <div class="search-box">
+            <form ng-submit="" method="POST">
+                <div class="search-input">
+                    <input type="text" placeholder="Company, Cat, Products">
+                    <input type="text" placeholder="Location">
+                    <!--<a href="#" class="btn1">Search</a>-->
+                    <input type="submit" class="btn1" name="submit" value="Submit">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
