53e57590e7b4647d5f3452b4e7731345b4ea6de1
khyati@123456 <khyati.aileensoul@gmail.com>
2018-02-06 19:10:55 +0530

    freelancer apply

--- application/front/models/Freelancer_apply_model.php
+++ application/front/models/Freelancer_apply_model.php
@@ -14,12 +14,19 @@ class Freelancer_apply_model extends CI_Model {
     }
     
     public function getfreelancerapplypost($user_id, $select_data) {
-        $select_data = "post_id,post_name,created_date,";
-        $this->db->select($select_data)->from('freelancer_post');
-        $this->db->where(array('is_delete' => '0', 'status' => '1'));
+//        $select_data = "post_id,post_name,created_date,username,fullname,post_rate,post_rating_type,post_currency,city,country,post_skill,post_description,post_field_req";
+        $select_data = "post_id,post_name,fp.created_date,post_rate,post_rating_type,currency_name as post_currency,ct.city_name as city,cr.country_name as country,post_skill,post_description,post_field_req";
+        $this->db->select($select_data)->from('freelancer_post fp');
+        $this->db->join('job_title jt', 'jt.title_id = fp.post_name', 'left');
+        $this->db->join('currency c', 'c.currency_id = fp.post_currency', 'left');
+         $this->db->join('cities ct', 'ct.city_id = fp.city', 'left');
+        $this->db->join('countries cr', 'cr.country_id = fp.country', 'left');
+        $this->db->where(array('fp.is_delete' => '0', 'fp.status' => '1'));
         $query = $this->db->get();
          $result_array = $query->result_array();
         return $result_array;
+        
+   
     }
     
     
