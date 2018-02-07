6b5b49d921c4569ed261c2836497637c9db74fc1
khyati@123456 <khyati.aileensoul@gmail.com>
2018-02-07 19:28:55 +0530

    frelancer apply model

--- application/front/models/Freelancer_apply_model.php
+++ application/front/models/Freelancer_apply_model.php
@@ -27,28 +27,31 @@ class Freelancer_apply_model extends CI_Model {
         $result_array = $query->result_array();
         return $result_array;
     }
-    
-    function searchFreelancerApplyData($keyword = '', $location = '',$time = '') {
-       
+
+    function searchFreelancerApplyData($keyword = '', $location = '', $time = '') {
+
         $keyword = str_replace('%20', ' ', $keyword);
         $location = str_replace('%20', ' ', $location);
-        
-      //  $busCat = $this->findBusinesCategory($keyword);
-        $this->db->select('post_id')->from('freelancer_post fp');
+
+        // $busCat = $this->findBusinesCategory($keyword);
+        $this->db->select('post_id,post_name,(SELECT  Count(uv.invite_id) As invitecount FROM ailee_user_invite as uv WHERE   (uv.post_id = fp.post_id) ) As ShortListedCount,(SELECT  Count(afa.app_id) As invitecount FROM ailee_freelancer_apply as afa WHERE (afa.post_id = fp.post_id) ) As AppliedCount,fp.created_date,post_rate,GROUP_CONCAT(DISTINCT(s.skill)) as post_skill,post_rating_type,currency_name as post_currency,ct.city_name as city,cr.country_name as country,post_description,post_field_req')->from('freelancer_post fp,ailee_skill s');
         $this->db->join('cities ct', 'ct.city_id = fp.city', 'left');
         $this->db->join('countries cr', 'cr.country_id = fp.country', 'left');
-        $this->db->join('states s', 's.state_name = fp.state', 'left');
+        $this->db->join('states st', 'st.state_name = fp.state', 'left');
         $this->db->join('freelancer_hire_reg fhg', 'fhg.user_id = fp.user_id', 'left');
+        $this->db->join('currency c', 'c.currency_id = fp.post_currency', 'left');
+        $this->db->where('FIND_IN_SET(s.skill_id, fp.`post_skill`) !=', 0);
         if ($keyword != '' && $busCat == '') {
             $this->db->where("(fp.post_name LIKE '%$keyword%' OR fp.post_description LIKE '%$keyword%')");
-        }elseif ($keyword != '' && $busCat != '') {
-            $this->db->where("(bp.company_name LIKE '%$keyword%' OR bp.address LIKE '%$keyword%' OR bp.contact_person LIKE '%$keyword%' OR bp.contact_mobile LIKE '%$keyword%' OR bp.contact_email LIKE '%$keyword%' OR bp.contact_website LIKE '%$keyword%' OR bp.details LIKE '%$keyword%' OR bp.business_slug LIKE '%$keyword%' OR bp.other_business_type LIKE '%$keyword%' OR bp.other_industrial LIKE '%$keyword%' OR bp.industriyal = '$busCat')");
+        } elseif ($keyword != '' && $busCat != '') {
+            $this->db->where("(fp.company_name LIKE '%$keyword%' OR fp.address LIKE '%$keyword%' OR fp.contact_person LIKE '%$keyword%' OR bp.contact_mobile LIKE '%$keyword%' OR bp.contact_email LIKE '%$keyword%' OR bp.contact_website LIKE '%$keyword%' OR bp.details LIKE '%$keyword%' OR bp.business_slug LIKE '%$keyword%' OR bp.other_business_type LIKE '%$keyword%' OR bp.other_industrial LIKE '%$keyword%' OR bp.industriyal = '$busCat')");
         }
         if ($location != '') {
-            $this->db->where("(ct.city_name = '$location' OR cr.country_name = '$location' OR s.state_name = '$location')");
+            $this->db->where("(ct.city_name = '$location' OR cr.country_name = '$location' OR st.state_name = '$location')");
         }
         $this->db->where('fp.status', '1');
         $this->db->where('fp.is_delete', '0');
+        $this->db->group_by('fp.post_skill');
         $query = $this->db->get();
         $result_array = $query->result_array();
         return $result_array;
